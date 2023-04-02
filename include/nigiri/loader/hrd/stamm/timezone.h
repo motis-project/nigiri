6e2d85e731e5002484f6fdc320460cba9d2db472
Felix Gündling <felix.guendling@gmail.com>
2023-04-02 21:48:58 +0200

    hrd: support multiple seasons (#25)

--- include/nigiri/loader/hrd/stamm/timezone.h
+++ include/nigiri/loader/hrd/stamm/timezone.h
@@ -14,14 +14,13 @@ using timezone_map_t =
     std::map<eva_number, std::pair<timezone_idx_t, tz_offsets>>;
 
 inline bool is_local_time_in_season(
-    tz_offsets const& tz,
+    tz_offsets::season const& season,
     unixtime_t const day,
     duration_t const local_minutes_after_midnight) {
   using std::chrono::sys_days;
   auto const local_time = day + local_minutes_after_midnight;
-  return tz.season_.has_value() &&
-         tz.season_->begin_ + tz.season_->season_begin_mam_ <= local_time &&
-         local_time < tz.season_->end_ + tz.season_->season_end_mam_;
+  return season.begin_ + season.season_begin_mam_ <= local_time &&
+         local_time < season.end_ + season.season_end_mam_;
 }
 
 inline std::tuple<duration_t, duration_t, bool> local_mam_to_utc_mam(
@@ -29,17 +28,27 @@ inline std::tuple<duration_t, duration_t, bool> local_mam_to_utc_mam(
     unixtime_t const day,
     duration_t const local_mam,
     bool const first = false) {
-  auto const is_season = is_local_time_in_season(tz, day, local_mam);
-  auto const tz_offset = is_season ? tz.season_->offset_ : tz.offset_;
+  auto const active_season_it =
+      utl::find_if(tz.seasons_, [&](tz_offsets::season const& season) {
+        return is_local_time_in_season(season, day, local_mam);
+      });
+  auto const tz_offset = active_season_it == end(tz.seasons_)
+                             ? tz.offset_
+                             : active_season_it->offset_;
   auto utc_mam = local_mam - tz_offset;
 
   auto const local_time = day + local_mam;
-  auto const local_season_begin = tz.season_->begin_ +
-                                  tz.season_->season_begin_mam_ +
-                                  (tz.season_->offset_ - tz.offset_);
-  auto const valid = !is_season || local_time > local_season_begin;
-  if (!valid) {
-    utc_mam += 1_hours;
+
+  auto valid = true;
+  auto const is_season = active_season_it != end(tz.seasons_);
+  if (is_season) {
+    auto const local_season_begin = active_season_it->begin_ +
+                                    active_season_it->season_begin_mam_ +
+                                    (active_season_it->offset_ - tz.offset_);
+    valid = local_time > local_season_begin;
+    if (!valid) {
+      utc_mam += 1_hours;
+    }
   }
 
   if (first && utc_mam >= 1_days) {
