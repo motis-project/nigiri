0469659f17ac9419b8fca10738cfa7d35bd0bbba
Felix Gündling <felix.guendling@gmail.com>
2023-04-20 13:06:37 +0200

    wip

--- src/routing/dijkstra.cc
+++ src/routing/dijkstra.cc
@@ -53,10 +53,10 @@ void dijkstra(timetable const& tt,
 
   dial<label, kMaxTravelTime, get_bucket> pq;
   for (auto const& [l, duration] : min) {
+    auto const d = duration;
     for_each_meta(tt, q.start_match_mode_, l, [&](location_idx_t const meta) {
-      // NOLINTNEXTLINE(clang-analyzer-core.CallAndMessage)
-      pq.push(label{meta, duration});
-      dists[to_idx(meta)] = std::min(duration, dists[to_idx(meta)]);
+      pq.push(label{meta, d});
+      dists[to_idx(meta)] = std::min(d, dists[to_idx(meta)]);
       trace("DIJKSTRA INIT @{}: {}\n", location{tt, meta}, duration);
     });
   }
