0fc69799ec79d3f0e960c1b9952d97dfb91702c3
Carsten Hagelgans <52030581+carstenhagelgans@users.noreply.github.com>
2023-06-12 16:20:39 +0200

    added profiles

---
+++ include/nigiri/profiles.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace nigiri {
+
+// Used for array indexes! Don't change the numbers!
+enum profile {
+  DEFAULT = 0,
+  // Insert new profiles here!
+  SIZE
+};
+
+}  // namespace nigiri
