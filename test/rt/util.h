c6fe1f8c39e5dcd977859832658f54c71af957c4
Felix Gündling <felix.guendling@gmail.com>
2023-06-30 22:30:35 +0200

    wip

--- test/rt/util.h
+++ test/rt/util.h
@@ -17,17 +17,18 @@ struct trip {
     std::optional<std::string> stop_id_{};
     std::optional<unsigned> seq_{};
     event_type ev_type_;
-    unsigned delay_minutes_{0U};
+    int delay_minutes_{0};
   };
   std::string trip_id_;
   std::vector<delay> delays_;
 };
 
 template <typename T>
-std::int64_t to_unix(T&& x) {
-  return std::chrono::time_point_cast<std::chrono::seconds>(x)
-      .time_since_epoch()
-      .count();
+std::uint64_t to_unix(T&& x) {
+  return static_cast<std::uint64_t>(
+      std::chrono::time_point_cast<std::chrono::seconds>(x)
+          .time_since_epoch()
+          .count());
 };
 
 transit_realtime::FeedMessage to_feed_msg(std::vector<trip> const& trip_delays,
