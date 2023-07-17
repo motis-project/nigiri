37e34fb3a525b2ca4764b8c0566798ff3d456399
Carsten Hagelgans <carsten.hagelgans@tu-darmstadt.de>
2023-07-17 08:11:35 +0200

    set profile in query as size_t

--- include/nigiri/routing/query.h
+++ include/nigiri/routing/query.h
@@ -44,7 +44,7 @@ struct query {
   unsigned min_connection_count_{0U};
   bool extend_interval_earlier_{false};
   bool extend_interval_later_{false};
-  uint16_t profile_{0};
+  std::size_t profile_{0};
 };
 
 }  // namespace nigiri::routing
