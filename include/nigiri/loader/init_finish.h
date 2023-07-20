bdcc2bf334970fbe0036e92ef590467ee2ed5950
Felix Gündling <felix.guendling@gmail.com>
2023-07-20 18:56:15 +0200

    wip

--- include/nigiri/loader/init_finish.h
+++ include/nigiri/loader/init_finish.h
@@ -7,6 +7,8 @@ struct timetable;
 namespace nigiri::loader {
 
 void register_special_stations(timetable&);
-void finalize(timetable&, bool const adjust_footpaths = false);
+void finalize(timetable&,
+              bool adjust_footpaths = false,
+              bool merge_duplicates = false);
 
 }  // namespace nigiri::loader
