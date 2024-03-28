c6d4fc6e40d380d35dde5565eac86471ac8298b2
mority <mority@posteo.net>
2024-03-28 18:43:36 +0100

    WIP

--- include/nigiri/query_generator/transport_mode.h
+++ include/nigiri/query_generator/transport_mode.h
@@ -7,19 +7,19 @@ namespace nigiri::query_generation {
 
 struct transport_mode {
   constexpr std::uint32_t range() const {
-    return speed_ * max_duration_ * 60U;
+    return speed_ * max_duration_;
   }  // [m]
 
   std::int32_t mode_id_;
-  std::uint16_t speed_;  // [m/s]
+  std::uint16_t speed_;  // [m/minute]
   std::uint16_t max_duration_;  // [minutes]
 };
 
 constexpr static transport_mode kWalk{
-    .mode_id_ = 1, .speed_ = 1U, .max_duration_ = 15};
+    .mode_id_ = 1, .speed_ = 50U, .max_duration_ = 15};
 constexpr static transport_mode kBicycle{
-    .mode_id_ = 2, .speed_ = 3U, .max_duration_ = 15};
+    .mode_id_ = 2, .speed_ = 200U, .max_duration_ = 15};
 constexpr static transport_mode kCar{
-    .mode_id_ = 3, .speed_ = 10, .max_duration_ = 15};
+    .mode_id_ = 3, .speed_ = 800U, .max_duration_ = 15};
 
 }  // namespace nigiri::query_generation
