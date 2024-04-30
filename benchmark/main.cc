4819b0f9eaaf755749c455bf7169eb753341853d
mority <mority@posteo.net>
2024-04-30 13:20:26 +0200

    clean up

--- benchmark/main.cc
+++ benchmark/main.cc
@@ -30,80 +30,6 @@ nigiri::pareto_set<nigiri::routing::journey> raptor_search(
                .journeys_);
 }
 
-std::unique_ptr<cista::wrapped<nigiri::timetable>> load_timetable(
-    std::filesystem::path const& input_path,
-    nigiri::interval<date::sys_days> const& tt_interval) {
-  auto load_timetable_timer = nigiri::scoped_timer("loading timetable");
-
-  // gather paths of input files in target folder
-  auto input_files = std::vector<std::filesystem::path>{};
-  if (std::filesystem::is_regular_file(input_path) &&
-      input_path.has_extension() && input_path.extension() == ".zip") {
-    // input path directly to GTFS zip file
-    input_files.emplace_back(input_path);
-  } else if (std::filesystem::is_directory(input_path)) {
-    // input path to directory
-    for (auto const& dir_entry :
-         std::filesystem::directory_iterator(input_path)) {
-      std::filesystem::path file_path(dir_entry);
-      if (std::filesystem::is_regular_file(dir_entry) &&
-          file_path.has_extension() && file_path.extension() == ".zip") {
-        input_files.emplace_back(file_path);
-      }
-    }
-  } else {
-    std::cout << "Error: timetable path provided is invalid\n";
-  }
-
-  auto const config = nigiri::loader::loader_config{100U, "Europe/Berlin"};
-
-  // hash tt settings and input files
-  auto h = cista::hash_combine(
-      cista::BASE_HASH, tt_interval.from_.time_since_epoch().count(),
-      tt_interval.to_.time_since_epoch().count(), config.link_stop_distance_,
-      cista::hash(config.default_tz_));
-  for (auto const& input_file : input_files) {
-    h = cista::hash_combine(h, cista::hash(input_file.string()));
-  }
-
-  auto const data_dir = std::filesystem::is_directory(input_files[0])
-                            ? input_files[0]
-                            : input_files[0].parent_path();
-  auto const dump_file_path = data_dir / fmt::to_string(h);
-
-  std::unique_ptr<cista::wrapped<nigiri::timetable>> tt;
-  auto loaded = false;
-
-  // try to load timetable from cache
-  if (exists(dump_file_path)) {
-    log(nigiri::log_lvl::info, "benchmark.load", "loading cached timetable {}",
-        fmt::to_string(h));
-    try {
-      tt = std::make_unique<cista::wrapped<nigiri::timetable>>(
-          nigiri::timetable::read(cista::memory_holder{
-              cista::file{dump_file_path.c_str(), "r"}.content()}));
-      (**tt).locations_.resolve_timezones();
-      loaded = true;
-    } catch (std::exception const& e) {
-      log(nigiri::log_lvl::error, "benchmark.load",
-          "can not read cached timetable image: {}", e.what());
-    }
-  }
-
-  // load timetable from GTFS files
-  if (!loaded) {
-    log(nigiri::log_lvl::info, "benchmark.load",
-        "no cached timetable found, loading from files");
-    tt = std::make_unique<cista::wrapped<nigiri::timetable>>(
-        cista::raw::make_unique<nigiri::timetable>(
-            load(input_files, config, tt_interval)));
-    // write cache file
-    (**tt).write(dump_file_path);
-  }
-
-  return tt;
-}
-
 std::vector<std::string> tokenize(std::string const& str,
                                   char delimiter,
                                   std::uint32_t n_tokens) {
@@ -119,20 +45,6 @@ std::vector<std::string> tokenize(std::string const& str,
     start = end + 1U;
   }
   return tokens;
-}
-
-std::optional<date::sys_days> parse_date(std::string const& str) {
-  using namespace std::chrono;
-
-  auto const date_regex = std::regex{"^[0-9]{4}-[0|1][0-9]-[0-3][0-9]$"};
-  if (!std::regex_match(begin(str), end(str), date_regex)) {
-    return std::nullopt;
-  }
-  auto const tokens = tokenize(str, '-', 3U);
-  return year_month_day{
-      year{std::stoi(tokens[0])},
-      month{static_cast<std::uint32_t>(std::stoul(tokens[1]))},
-      day{static_cast<std::uint32_t>(std::stoul(tokens[2]))}};
 }
 
 std::optional<geo::box> parse_bbox(std::string const& str) {
