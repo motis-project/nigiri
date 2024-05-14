c24fb8fea5fa17c5bde43b02ad7ea823d3886612
mority <mority@posteo.net>
2024-05-14 17:07:33 +0200

    wip

--- include/nigiri/routing/search.h
+++ include/nigiri/routing/search.h
@@ -309,9 +309,13 @@ private:
 
   void add_start_labels(start_time_t const& start_interval,
                         bool const add_ontrip) {
+    state_.starts_.reserve(500'000);
     get_starts(SearchDir, tt_, rtt_, start_interval, q_.start_,
                q_.start_match_mode_, q_.use_start_footpaths_, state_.starts_,
                add_ontrip, q_.prf_idx_);
+    std::sort(
+        begin(state_.starts_), end(state_.starts_),
+        [&](start const& a, start const& b) { return kFwd ? b < a : a < b; });
   }
 
   void remove_ontrip_results() {
