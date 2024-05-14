0c51ee82bbc06174df28a25bfb7380322b813da2
Moritz Fischer <57353722+mority@users.noreply.github.com>
2024-05-14 17:40:25 +0200

    Fastest direct dijkstra (#94)
    --------
    
    Co-authored-by: Felix Gündling <felix.guendling@gmail.com>

--- include/nigiri/routing/get_fastest_direct.h
+++ include/nigiri/routing/get_fastest_direct.h
@@ -1,11 +1,16 @@
 #pragma once
 
+#include "nigiri/routing/dijkstra.h"
 #include "nigiri/routing/query.h"
 #include "nigiri/timetable.h"
 #include "nigiri/types.h"
 
 namespace nigiri::routing {
 
-duration_t get_fastest_direct(timetable const&, query const&, direction const);
+duration_t get_fastest_direct(
+    timetable const&,
+    query const&,
+    direction const,
+    label::dist_t const max_dist = std::numeric_limits<label::dist_t>::max());
 
 }  // namespace nigiri::routing
