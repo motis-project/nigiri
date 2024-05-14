daccb37b0ac7918cf323d01a678d1ce6cda32f56
Felix Gündling <felix.guendling@gmail.com>
2024-05-14 15:33:40 +0200

    Update limits.h

--- include/nigiri/routing/limits.h
+++ include/nigiri/routing/limits.h
@@ -8,7 +8,7 @@ namespace nigiri::routing {
 
 static constexpr auto const kMaxTransfers = std::uint8_t{7U};
 static constexpr auto const kMaxTravelTime = 1_days;
-constexpr auto const kMaxSearchIntervalSize =
+static constexpr auto const kMaxSearchIntervalSize =
     date::days{std::numeric_limits<duration_t::rep>::max() / 1440} -
     kMaxTravelTime;
 
