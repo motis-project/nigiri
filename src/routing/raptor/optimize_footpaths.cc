fca064a7b138d8633130a8f9f2f845ea836c9741
mority <mority@posteo.net>
2024-06-05 09:46:17 +0200

    uses frun, removes lambda

--- src/routing/raptor/optimize_footpaths.cc
+++ src/routing/raptor/optimize_footpaths.cc
@@ -25,41 +25,35 @@ void optimize_start(timetable const& tt,
       continue;
     }
 
-    auto const iterate_stops = [&](auto&& stop_seq) {
-      for (auto stop_idx = stop_idx_t{0U}; stop_idx != ree.stop_range_.to_ - 1U;
-           ++stop_idx) {
-        auto stp = stop{stop_seq[stop_idx]};
-        if (!stp.in_allowed() || !matches(tt, location_match_mode::kExact,
-                                          o.target(), stp.location_idx())) {
-          continue;
-        }
-        auto const dep =
-            rtt != nullptr &&
-                    rtt->resolve_rt(ree.r_.t_) != rt_transport_idx_t::invalid()
-                ? rtt->unix_event_time(rtt->resolve_rt(ree.r_.t_), stop_idx,
-                                       event_type::kDep)
-                : tt.event_time(ree.r_.t_, stop_idx, event_type::kDep);
-        auto const o_start = dep - o.duration();
-        if (offset_leg.dep_time_ <= o_start) {
-          offset_leg.to_ = stp.location_idx();
-          offset_leg.dep_time_ = o_start;
-          offset_leg.arr_time_ = dep;
-          offset_leg.uses_ = o;
-          transport_leg.from_ = stp.location_idx();
-          transport_leg.dep_time_ = dep;
-          ree.stop_range_.from_ = stop_idx;
-          offset_dur_best = o.duration();
-        }
-      }
-    };
+    auto const fr = (rtt != nullptr && rtt->resolve_rt(ree.r_.t_) !=
+                                           rt_transport_idx_t::invalid())
+                        ? rt::frun::from_rt(tt, rtt, rtt->resolve_rt(ree.r_.t_))
+                        : rt::frun::from_t(tt, rtt, ree.r_.t_);
 
-    if (rtt != nullptr &&
-        rtt->resolve_rt(ree.r_.t_) != rt_transport_idx_t::invalid()) {
-      iterate_stops(
-          rtt->rt_transport_location_seq_[rtt->resolve_rt(ree.r_.t_)]);
-    } else {
-      iterate_stops(
-          tt.route_location_seq_[tt.transport_route_[ree.r_.t_.t_idx_]]);
+    for (auto stop_idx = stop_idx_t{0U}; stop_idx != ree.stop_range_.to_ - 1U;
+         ++stop_idx) {
+      auto stp = fr[stop_idx];
+      if (!stp.in_allowed() || !matches(tt, location_match_mode::kExact,
+                                        o.target(), stp.get_location_idx())) {
+        continue;
+      }
+      auto const dep =
+          rtt != nullptr &&
+                  rtt->resolve_rt(ree.r_.t_) != rt_transport_idx_t::invalid()
+              ? rtt->unix_event_time(rtt->resolve_rt(ree.r_.t_), stop_idx,
+                                     event_type::kDep)
+              : tt.event_time(ree.r_.t_, stop_idx, event_type::kDep);
+      auto const o_start = dep - o.duration();
+      if (offset_leg.dep_time_ <= o_start) {
+        offset_leg.to_ = stp.get_location_idx();
+        offset_leg.dep_time_ = o_start;
+        offset_leg.arr_time_ = dep;
+        offset_leg.uses_ = o;
+        transport_leg.from_ = stp.get_location_idx();
+        transport_leg.dep_time_ = dep;
+        ree.stop_range_.from_ = stop_idx;
+        offset_dur_best = o.duration();
+      }
     }
   }
 }
@@ -81,41 +75,35 @@ void optimize_end(timetable const& tt,
       continue;
     }
 
-    auto const iterate_stops = [&](auto&& stop_seq) {
-      for (auto stop_idx = static_cast<stop_idx_t>(ree.stop_range_.from_ + 1U);
-           stop_idx != stop_seq.size(); ++stop_idx) {
-        auto stp = stop{stop_seq[stop_idx]};
-        if (!stp.out_allowed() || !matches(tt, location_match_mode::kExact,
-                                           o.target(), stp.location_idx())) {
-          continue;
-        }
-        auto const arr =
-            rtt != nullptr &&
-                    rtt->resolve_rt(ree.r_.t_) != rt_transport_idx_t::invalid()
-                ? rtt->unix_event_time(rtt->resolve_rt(ree.r_.t_), stop_idx,
-                                       event_type::kArr)
-                : tt.event_time(ree.r_.t_, stop_idx, event_type::kArr);
-        auto const o_end = arr + o.duration();
-        if (o_end <= offset_leg.arr_time_) {
-          offset_leg.from_ = stp.location_idx();
-          offset_leg.dep_time_ = arr;
-          offset_leg.arr_time_ = o_end;
-          offset_leg.uses_ = o;
-          transport_leg.to_ = stp.location_idx();
-          transport_leg.arr_time_ = arr;
-          ree.stop_range_.to_ = stop_idx + 1U;
-          offset_dur_best = o.duration();
-        }
-      }
-    };
+    auto const fr = (rtt != nullptr && rtt->resolve_rt(ree.r_.t_) !=
+                                           rt_transport_idx_t::invalid())
+                        ? rt::frun::from_rt(tt, rtt, rtt->resolve_rt(ree.r_.t_))
+                        : rt::frun::from_t(tt, rtt, ree.r_.t_);
 
-    if (rtt != nullptr &&
-        rtt->resolve_rt(ree.r_.t_) != rt_transport_idx_t::invalid()) {
-      iterate_stops(
-          rtt->rt_transport_location_seq_[rtt->resolve_rt(ree.r_.t_)]);
-    } else {
-      iterate_stops(
-          tt.route_location_seq_[tt.transport_route_[ree.r_.t_.t_idx_]]);
+    for (auto stop_idx = static_cast<stop_idx_t>(ree.stop_range_.from_ + 1U);
+         stop_idx != fr.size(); ++stop_idx) {
+      auto stp = fr[stop_idx];
+      if (!stp.out_allowed() || !matches(tt, location_match_mode::kExact,
+                                         o.target(), stp.get_location_idx())) {
+        continue;
+      }
+      auto const arr =
+          rtt != nullptr &&
+                  rtt->resolve_rt(ree.r_.t_) != rt_transport_idx_t::invalid()
+              ? rtt->unix_event_time(rtt->resolve_rt(ree.r_.t_), stop_idx,
+                                     event_type::kArr)
+              : tt.event_time(ree.r_.t_, stop_idx, event_type::kArr);
+      auto const o_end = arr + o.duration();
+      if (o_end <= offset_leg.arr_time_) {
+        offset_leg.from_ = stp.get_location_idx();
+        offset_leg.dep_time_ = arr;
+        offset_leg.arr_time_ = o_end;
+        offset_leg.uses_ = o;
+        transport_leg.to_ = stp.get_location_idx();
+        transport_leg.arr_time_ = arr;
+        ree.stop_range_.to_ = stop_idx + 1U;
+        offset_dur_best = o.duration();
+      }
     }
   }
 }
@@ -139,85 +127,69 @@ void optimize_transfers(timetable const& tt,
     auto& ree = get<journey::run_enter_exit>(leg.uses_);
     auto& ree_transfer = get<journey::run_enter_exit>(leg_transfer.uses_);
 
-    auto const iterate_stops = [&](auto&& stop_seq) {
-      for (auto stop_idx = static_cast<stop_idx_t>(ree.stop_range_.from_ + 1U);
-           stop_idx != stop_seq.size(); ++stop_idx) {
-        auto const stp = stop{stop_seq[stop_idx]};
-        if (!stp.out_allowed()) {
+    auto const fr = (rtt != nullptr && rtt->resolve_rt(ree.r_.t_) !=
+                                           rt_transport_idx_t::invalid())
+                        ? rt::frun::from_rt(tt, rtt, rtt->resolve_rt(ree.r_.t_))
+                        : rt::frun::from_t(tt, rtt, ree.r_.t_);
+    auto const fr_transfer =
+        (rtt != nullptr &&
+         rtt->resolve_rt(ree_transfer.r_.t_) != rt_transport_idx_t::invalid())
+            ? rt::frun::from_rt(tt, rtt, rtt->resolve_rt(ree_transfer.r_.t_))
+            : rt::frun::from_t(tt, rtt, ree_transfer.r_.t_);
+
+    for (auto stop_idx = static_cast<stop_idx_t>(ree.stop_range_.from_ + 1U);
+         stop_idx != fr.size(); ++stop_idx) {
+      auto const stp = fr[stop_idx];
+      if (!stp.out_allowed()) {
+        continue;
+      }
+      auto const footpaths =
+          tt.locations_.footpaths_out_[q.prf_idx_][stp.get_location_idx()];
+      for (auto const& fp : footpaths) {
+        if (fp.duration() >= fp_dur_best) {
           continue;
         }
-        auto const footpaths =
-            tt.locations_.footpaths_out_[q.prf_idx_][stp.location_idx()];
-        for (auto const& fp : footpaths) {
-          if (fp.duration() >= fp_dur_best) {
+
+        for (auto stop_idx_transfer = stop_idx_t{0U};
+             stop_idx_transfer != ree_transfer.stop_range_.to_ - 1U;
+             ++stop_idx_transfer) {
+          auto const stp_transfer = fr_transfer[stop_idx_transfer];
+          if (!stp_transfer.in_allowed() ||
+              !matches(tt, location_match_mode::kExact, fp.target(),
+                       stp_transfer.get_location_idx())) {
             continue;
           }
-
-          auto const iterate_stops_transfer = [&](auto&& stop_seq_transfer) {
-            for (auto stop_idx_transfer = stop_idx_t{0U};
-                 stop_idx_transfer != ree_transfer.stop_range_.to_ - 1U;
-                 ++stop_idx_transfer) {
-              auto const stp_transfer =
-                  stop{stop_seq_transfer[stop_idx_transfer]};
-              if (!stp_transfer.in_allowed() ||
-                  !matches(tt, location_match_mode::kExact, fp.target(),
-                           stp_transfer.location_idx())) {
-                continue;
-              }
-              auto const arr =
-                  rtt != nullptr && rtt->resolve_rt(ree.r_.t_) !=
-                                        rt_transport_idx_t::invalid()
-                      ? rtt->unix_event_time(rtt->resolve_rt(ree.r_.t_),
-                                             stop_idx, event_type::kArr)
-                      : tt.event_time(ree.r_.t_, stop_idx, event_type::kArr);
-              auto const arr_fp = arr + fp.duration();
-              auto const dep =
-                  rtt != nullptr && rtt->resolve_rt(ree_transfer.r_.t_) !=
-                                        rt_transport_idx_t::invalid()
-                      ? rtt->unix_event_time(
-                            rtt->resolve_rt(ree_transfer.r_.t_),
-                            stop_idx_transfer, event_type::kDep)
-                      : tt.event_time(ree_transfer.r_.t_, stop_idx_transfer,
-                                      event_type::kDep);
-              if (arr_fp <= dep) {
-                leg.to_ = stp.location_idx();
-                leg.arr_time_ = arr;
-                ree.stop_range_.to_ = stop_idx + 1U;
-                leg_footpath.from_ = stp.location_idx();
-                leg_footpath.to_ = stp_transfer.location_idx();
-                leg_footpath.dep_time_ = arr;
-                leg_footpath.arr_time_ = arr_fp;
-                leg_footpath.uses_ = fp;
-                leg_transfer.from_ = stp_transfer.location_idx();
-                leg_transfer.dep_time_ = dep;
-                ree_transfer.stop_range_.from_ = stop_idx_transfer;
-                fp_dur_best = fp.duration();
-              }
-              break;
-            }
-          };
-
-          if (rtt != nullptr && rtt->resolve_rt(ree_transfer.r_.t_) !=
-                                    rt_transport_idx_t::invalid()) {
-            iterate_stops_transfer(
-                rtt->rt_transport_location_seq_[rtt->resolve_rt(
-                    ree_transfer.r_.t_)]);
-          } else {
-            iterate_stops_transfer(
-                tt.route_location_seq_
-                    [tt.transport_route_[ree_transfer.r_.t_.t_idx_]]);
+          auto const arr =
+              rtt != nullptr && rtt->resolve_rt(ree.r_.t_) !=
+                                    rt_transport_idx_t::invalid()
+                  ? rtt->unix_event_time(rtt->resolve_rt(ree.r_.t_), stop_idx,
+                                         event_type::kArr)
+                  : tt.event_time(ree.r_.t_, stop_idx, event_type::kArr);
+          auto const arr_fp = arr + fp.duration();
+          auto const dep =
+              rtt != nullptr && rtt->resolve_rt(ree_transfer.r_.t_) !=
+                                    rt_transport_idx_t::invalid()
+                  ? rtt->unix_event_time(rtt->resolve_rt(ree_transfer.r_.t_),
+                                         stop_idx_transfer, event_type::kDep)
+                  : tt.event_time(ree_transfer.r_.t_, stop_idx_transfer,
+                                  event_type::kDep);
+          if (arr_fp <= dep) {
+            leg.to_ = stp.get_location_idx();
+            leg.arr_time_ = arr;
+            ree.stop_range_.to_ = stop_idx + 1U;
+            leg_footpath.from_ = stp.get_location_idx();
+            leg_footpath.to_ = stp_transfer.get_location_idx();
+            leg_footpath.dep_time_ = arr;
+            leg_footpath.arr_time_ = arr_fp;
+            leg_footpath.uses_ = fp;
+            leg_transfer.from_ = stp_transfer.get_location_idx();
+            leg_transfer.dep_time_ = dep;
+            ree_transfer.stop_range_.from_ = stop_idx_transfer;
+            fp_dur_best = fp.duration();
           }
+          break;
         }
       }
-    };
-
-    if (rtt != nullptr &&
-        rtt->resolve_rt(ree.r_.t_) != rt_transport_idx_t::invalid()) {
-      iterate_stops(
-          rtt->rt_transport_location_seq_[rtt->resolve_rt(ree.r_.t_)]);
-    } else {
-      iterate_stops(
-          tt.route_location_seq_[tt.transport_route_[ree.r_.t_.t_idx_]]);
     }
   }
 }
