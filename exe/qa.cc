b0dcd84461f4b2dde4a7b16590eaba93639bdca3
mority <mority@posteo.net>
2024-06-11 16:16:50 +0200

    qa bin

--- exe/qa.cc
+++ exe/qa.cc
@@ -7,6 +7,42 @@
 namespace fs = std::filesystem;
 namespace bpo = boost::program_options;
 
+// needs sorted vector
+template <typename T>
+T quantile(std::vector<T> const& v, double q) {
+  q = q < 0.0 ? 0.0 : q;
+  q = 1.0 < q ? 1.0 : q;
+  if (q == 1.0) {
+    return v.back();
+  }
+  return v[static_cast<std::size_t>(v.size() * q)];
+}
+
+void print_result(std::vector<std::pair<nigiri::qa::rating_t,
+                                        std::chrono::milliseconds>> const& var,
+                  std::string const& var_name) {
+  auto const pair_str = [](auto const& p) {
+    auto ss = std::stringstream{};
+    ss << "(" << p.first << ", " << p.second << ")";
+    return ss.str();
+  };
+
+  std::cout << "\n--- " << var_name << " --- (n = " << var.size() << ")"
+            << "\n  10%: " << pair_str(quantile(var, 0.1))
+            << "\n  20%: " << pair_str(quantile(var, 0.2))
+            << "\n  30%: " << pair_str(quantile(var, 0.3))
+            << "\n  40%: " << pair_str(quantile(var, 0.4))
+            << "\n  50%: " << pair_str(quantile(var, 0.5))
+            << "\n  60%: " << pair_str(quantile(var, 0.6))
+            << "\n  70%: " << pair_str(quantile(var, 0.7))
+            << "\n  80%: " << pair_str(quantile(var, 0.8))
+            << "\n  90%: " << pair_str(quantile(var, 0.9))
+            << "\n  99%: " << pair_str(quantile(var, 0.99))
+            << "\n99.9%: " << pair_str(quantile(var, 0.999))
+            << "\n  max: " << pair_str(var.back())
+            << "\n----------------------------------\n";
+}
+
 int main(int ac, char** av) {
   auto in_r = fs::path{};
   auto in_c = fs::path{};
@@ -15,9 +51,9 @@ int main(int ac, char** av) {
   desc.add_options()  //
       ("help,h", "produce this help message")  //
       ("reference,r", bpo::value(&in_r),
-       "input path a, binary dump of vector<pareto_set<journey>>")  //
+       "path to binary dump of vector<pareto_set<journey>>")  //
       ("compare,c", bpo::value(&in_c),
-       "input path b, binary dump of vector<pareto_set<journey>>");
+       "path to binary dump of vector<pareto_set<journey>>");
   auto const pos = bpo::positional_options_description{}.add("in", -1);
 
   auto vm = bpo::variables_map{};
@@ -64,6 +100,14 @@ int main(int ac, char** av) {
       }
     }
   }
+
+  std::sort(begin(rating_timing), end(rating_timing),
+            [](auto const& a, auto const& b) { return a.first < b.first; });
+  print_result(rating_timing, "rating");
+
+  std::sort(begin(rating_timing), end(rating_timing),
+            [](auto const& a, auto const& b) { return a.second < b.second; });
+  print_result(rating_timing, "timing");
 
   return 0;
 }
