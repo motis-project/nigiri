90c5012f486458b5e3a7e5d73d23f4b91810558a
Pablo Hoch <pablohoch@users.noreply.github.com>
2024-06-14 15:53:24 +0200

    Bike transport (#108)

--- include/nigiri/routing/query.h
+++ include/nigiri/routing/query.h
@@ -50,6 +50,7 @@ struct query {
   bool extend_interval_later_{false};
   profile_idx_t prf_idx_{0};
   clasz_mask_t allowed_claszes_{all_clasz_allowed()};
+  bool require_bike_transport_{false};
 };
 
 }  // namespace nigiri::routing
