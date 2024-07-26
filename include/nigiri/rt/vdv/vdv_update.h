06fd611f91c07c949b9807796bd3eb91967939d7
mority <mority@posteo.net>
2024-07-26 10:50:53 +0200

    wip

--- include/nigiri/rt/vdv/vdv_update.h
+++ include/nigiri/rt/vdv/vdv_update.h
@@ -16,8 +16,6 @@ struct statistics {
     out << "unsupported additional run: " << s.unsupported_additional_run_
         << "\nunsupported cancelled run: " << s.unsupported_cancelled_run_
         << "\nunsupported additional stop: " << s.unsupported_additional_stop_
-        << "\nrun with additional stops only: "
-        << s.run_with_additional_stops_only_
         << "\nunknown stop id: " << s.unknown_stop_id_
         << "\nfound stop but no transport: " << s.found_stop_but_no_transport_
         << "\nunmatchable run: " << s.unmatchable_run_
@@ -32,8 +30,6 @@ struct statistics {
         lhs.unsupported_additional_run_ += rhs.unsupported_additional_run_,
         lhs.unsupported_cancelled_run_ += rhs.unsupported_cancelled_run_,
         lhs.unsupported_additional_stop_ += rhs.unsupported_additional_run_,
-        lhs.run_with_additional_stops_only_ +=
-        rhs.run_with_additional_stops_only_,
         lhs.unknown_stop_id_ += rhs.unknown_stop_id_,
         lhs.found_stop_but_no_transport_ += rhs.found_stop_but_no_transport_,
         lhs.unmatchable_run_ += rhs.unmatchable_run_,
@@ -45,7 +41,6 @@ struct statistics {
   std::uint32_t unsupported_additional_run_{0U};
   std::uint32_t unsupported_cancelled_run_{0U};
   std::uint32_t unsupported_additional_stop_{0U};
-  std::uint32_t run_with_additional_stops_only_{0U};
   std::uint32_t unknown_stop_id_{0};
   std::uint32_t found_stop_but_no_transport_{0};
   std::uint32_t unmatchable_run_{0U};
