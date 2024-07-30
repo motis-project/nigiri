703a793ead368547074104c64ffc21decc6f4cb0
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2024-07-30 14:32:07 +0000

    WIP: Cleanup writer

--- dev/writer.cc
+++ dev/writer.cc
@@ -2,15 +2,12 @@
 #include <cista/mmap.h>
 #include <sys/types.h>
 #include <cstddef>
-#include <algorithm>
 #include <format>
 #include <iostream>
 #include <numeric>
-#include <ranges>
 #include <stdexcept>
 #include <string_view>
 #include <unordered_map>
-#include <vector>
 
 #include "cista/mmap.h"
 #include "utl/parser/buf_reader.h"
@@ -48,26 +45,6 @@ struct ShapePoint {
     utl::csv_col<size_t, UTL_NAME("shape_pt_sequence")> seq;
   };
 
-  static constexpr std::string_view sep{","};
-
-  static constexpr ShapePoint from_string(const std::string_view row) {
-    auto to_string_view = [](auto item) { return std::string_view(item); };
-    auto item_view =
-        std::views::split(row, sep) | std::views::transform(to_string_view);
-    std::vector<std::string_view> items{item_view.begin(), item_view.end()};
-    if (items.size() < 4 || items.size() > 5) {
-      throw InvalidShapesFormat{std::format("Invalid format in row '{}'", row)};
-    }
-    assert(items.size() == 4);
-
-    return ShapePoint{
-        static_cast<id_type>(std::stoul(items[0].data())),
-        double_to_fix(std::stod(items[1].data())),
-        double_to_fix(std::stod(items[2].data())),
-        std::stoul(items[3].data()),
-    };
-  }
-
   static constexpr ShapePoint from_shape(const Shape& shape) {
     return ShapePoint{
         shape.id.val(),
@@ -81,29 +58,6 @@ struct ShapePoint {
     return std::format("ShapePoint(id={}, {}, {}, seq={})", id, lat, lon, seq);
   }
 };
-
-auto read_lines(auto& data_source) {
-
-  auto filter_cr = [](const char c) { return c != '\r'; };
-  auto pred = [](const char curr, const char next) {
-    return curr != '\n' and next != '\n';
-  };
-  auto not_empty = [](const auto& view) { return view.begin() != view.end(); };
-  auto starts_with_int = [](const auto& view) {
-    char first = view.front();
-    return first >= '0' && first <= '9';
-  };
-  auto join = [](const auto& view) {
-    return std::string{view.begin(), view.end()};
-  };
-
-  return data_source | std::views::filter(filter_cr) |
-         std::views::chunk_by(pred) | std::views::transform(join) |
-         std::views::filter(not_empty) |
-         std::views::filter(starts_with_int)
-         // | std::views::take(200'000)
-         | std::views::transform(ShapePoint::from_string);
-}
 
 void progress_lines(const auto& file_content, auto func) {
   auto const progress_tracker = utl::activate_progress_tracker("writer");
@@ -129,13 +83,11 @@ auto get_cache(cista::mmap::protection mode) {
 
 auto get_cache_writer() { return get_cache(cista::mmap::protection::WRITE); }
 
-auto get_mapper(cista::mmap::protection mode)  {
+auto get_mapper(cista::mmap::protection mode) {
   return cista::mmap_vec<id_type>{cista::mmap{id_map_file.data(), mode}};
 }
 
-auto get_map_writer() {
-  return get_mapper(cista::mmap::protection::WRITE);
-}
+auto get_map_writer() { return get_mapper(cista::mmap::protection::WRITE); }
 
 void show_stats(auto& cache) {
   std::cout << std::format("Added {} buckets", cache.size()) << std::endl;
@@ -143,21 +95,15 @@ void show_stats(auto& cache) {
       std::accumulate(cache.begin(), cache.end(), 0u,
                       [](auto count, auto b) { return count + b.size(); });
   std::cout << std::format("Number of entries: {}", entries) << std::endl;
-
 }
 
 void store_ids(auto ids) {
   auto mapper = get_map_writer();
-  std::cout << "ID size: " << ids.size() << std::endl;
   mapper.reserve(static_cast<unsigned int>(2u * ids.size()));
-  auto count{0u};
   for (auto [id, pos] : ids) {
     mapper.push_back(id);
     mapper.push_back(pos);
-    ++count;
   }
-  std::cout << "Count: " << count << std::endl;
-  std::cout << "Size: " << mapper.size() << "/" << mapper.used_size_<< std::endl;
 }
 
 int main() {
@@ -181,22 +127,16 @@ int main() {
     bucket.push_back(point.lat);
     bucket.push_back(point.lon);
   };
-  constexpr bool custom{false};
-  const std::string_view s{shaped_data};
-  if (custom) {
-    std::cout << "Ranges" << std::endl;
-    std::ranges::for_each(read_lines(s), store_entry);
-    // std::ranges::for_each(read_lines(shaped_data), store_entry);
-  } else {
-    std::cout << "Parser" << std::endl;
-    progress_lines(s, store_entry);
-    // progress_lines(std::string_view(shaped_data), store_entry);
-  }
+  progress_lines(std::string_view(shaped_data), store_entry);
+
+  store_ids(ids);
 
   show_stats(cache);
+  std::cout << "Testing some ids …" << std::endl;
   for (auto key : {1, 134, 573}) {
-    std::cout << std::format("Key {} at position {}", key, ids.at(static_cast<id_type>(key))) << std::endl;
+    std::cout << std::format("Key {:>5} at position {:>5}", key,
+                             ids.at(static_cast<id_type>(key)))
+              << std::endl;
   }
-  store_ids(ids);
   return 0;
 }
