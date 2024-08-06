cbe3ce866b4d3e934bb272ab03b3e35abd284fb1
Felix Gündling <felix.guendling@gmail.com>
2024-08-06 09:52:29 +0200

    wip

--- include/nigiri/loader/assistance.h
+++ include/nigiri/loader/assistance.h
@@ -8,12 +8,15 @@
 
 namespace nigiri::loader {
 
-struct station {
-  std::string name_;
-  geo::latlng pos_;
-  oh::rule availability_;
+using assistance_idx_t = cista::strong<std::uint32_t, struct assistance_idx_>;
+
+struct assistance_times {
+  vecvec<assistance_idx_t, char> names_;
+  vector_map<assistance_idx_t, geo::latlng> pos_;
+  vector_map<assistance_idx_t, oh::ruleset_t> rules_;
+  vector_map<location_idx_t, assistance_idx_t> location_assistance_;
 };
 
-std::vector<station> read_availability(std::string_view);
+assistance_times read_assistance(std::string_view);
 
 }  // namespace nigiri::loader
--- src/loader/assistance.cc
+++ src/loader/assistance.cc
@@ -3,12 +3,13 @@
 #include "utl/parser/buf_reader.h"
 #include "utl/parser/csv_range.h"
 #include "utl/parser/line_range.h"
-#include "utl/pipes/transform.h"
+#include "utl/pipes/for_each.h"
 #include "utl/pipes/vec.h"
+#include "utl/progress_tracker.h"
 
 namespace nigiri::loader {
 
-void read_availability(timetable const& tt, std::string_view content) {
+void read_availability(timetable const& tt, std::string_view file_content) {
   struct assistance {
     utl::csv_col<utl::cstr, UTL_NAME("name")> name_;
     utl::csv_col<double, UTL_NAME("lat")> lat_;
@@ -16,23 +17,17 @@ void read_availability(timetable const& tt, std::string_view content) {
     utl::csv_col<utl::cstr, UTL_NAME("time")> time_;
   };
 
+  auto const a = assistance{};
   auto const progress_tracker = utl::get_active_progress_tracker();
-  progress_tracker->status("Parse Agencies")
+  progress_tracker->status("Parse Assistance")
       .out_bounds(0.F, 1.F)
       .in_high(file_content.size());
   return utl::line_range{utl::make_buf_reader(
              file_content, progress_tracker->update_fn())}  //
-         | utl::csv<agency>()  //
-         | utl::transform([&](agency const& a) {
-             return std::pair{
-                 a.id_->to_str(),
-                 tt.register_provider(
-                     {a.id_->view(), a.name_->view(),
-                      get_tz_idx(tt, timezones, a.tz_name_->trim().view())})};
-           })  //
-         | utl::to<agency_map_t>();
+         | utl::csv<assistance>()  //
+         | utl::for_each([&](assistance const& a) {
 
-  return {};
+           });
 }
 
 }  // namespace nigiri::loader
