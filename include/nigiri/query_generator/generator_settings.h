14dd54b7a25240c55586fc1c8e89aafab1f6b0d4
Moritz Fischer <57353722+mority@users.noreply.github.com>
2024-08-14 15:36:46 +0200

    Benchmark (#80)
    * WIP
    
    * WIP
    
    * new geo version
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP use weights for hour of time of day when randomizing time
    
    * WIP
    
    * WIP random_on_trip
    
    * WIP random_on_trip
    
    * WIP
    
    * WIP
    
    * WIP
    
    * refactoring, clean up
    
    * clean up
    
    * replaces unsigned long with std::uint64_t
    
    * clang-tidy
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Requested changes from review of PR #79
    
    * MSVC compatibility
    
    * whitespace
    
    * Requested changes from review of PR #79
    
    * loading timetable via load.h from rust branch
    
    * load timetable from cache file
    
    * query generation & processing
    
    * progress bars
    
    * WIP
    
    * WIP
    
    * test for reproducibility using seed value for RNG
    
    * user provided seed for RNG to reproduce queries
    
    * provide seed as cmd line arg, more stats, pretty printing
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * all control paths return a value
    
    * indent
    
    * WIP program options
    
    * WIP program options
    
    * print settings
    
    * print transport_mode
    
    * program_options
    
    * bbox support
    
    * bbox support, start and end date arguments
    
    * execute routing in try-catch
    
    * adds boost dependency
    
    * fixes boost program options lib name
    
    * refactoring
    
    * better bounding box support
    
    * clean up
    
    * brings back random_transport_active_stop, needed for ontrip query generation in motis
    
    * reduced num_queries to speedup test
    
    * some input checks
    
    * reformat
    
    * serial query generation since query generator is not thread safe
    
    * load.h/load.cc from rust branch
    
    * wip
    
    * adds boost dependency
    
    * adds importer executable
    
    * fixes error unformattable
    
    * wip
    
    * use binary timetable files created by importer
    
    * reuse allocation of search and raptor state
    
    * parse date using date lib IO
    
    * clean up
    
    * reformat
    
    * operator== for query
    
    * cleanup
    
    * remove allocations
    
    * clang-format-16
    
    * requested changes
    
    * formatting
    
    * wip
    
    * set start/dest coordinate
    
    * show other attributes when printing stats of an attribute
    
    * new boost version
    
    * new geo version
    
    * fix offset duration calculation
    
    * fix accessing routes of location with no routes
    
    * removes cout
    
    * wip
    
    * wip
    
    * more stats, print ranking of slowest queries
    
    * interval size unit hours
    
    * pretty printing
    
    * remove space between lat and lon for easier c&p
    
    * adds units to time designations
    
    * kMaxTransfers as default value, allow 2 digit input for max. number of transfers
    
    * increment progress tracker
    
    * re-adds benchmark target
    
    * print memory usage
    
    * print interval size in hours on macos
    
    * clang-format-17
    
    * print command to re-run slowest source-destination combination
    
    * cleanup
    
    * mv to exe dir
    
    * refactoring
    
    * revert undesired formatting
    
    * requested changes from review
    
    * adds build targets variable, add nigiri-benchmark as build target
    
    * make local include stand out
    
    * rm unnecessary comment
    
    * clang-tidy
    
    * clang-format-17
    
    * build targets variable in powershell
    
    * no var
    
    * wip
    
    * generic string, profile index limits
    
    * print double seconds on macos
    
    * print on macos
    
    * fix re-run cmd
    
    * fix re-run cmd
    
    * invalid start coord
    
    * use geo::latlng printing
    
    * to_string for predefined transport modes
    
    * fix re-run command
    
    * adds description of coordinates input format
    
    * link benchmark bin against ianatzdb-res to prevent crashes on windows
    
    * prevents crash if no location reachable from start/dest coord with given transport mode
    
    * print the query index when listing the 10 slowest queries
    
    ---------
    
    Co-authored-by: Felix Gündling <felix.guendling@gmail.com>

--- include/nigiri/query_generator/generator_settings.h
+++ include/nigiri/query_generator/generator_settings.h
@@ -1,21 +1,80 @@
 #pragma once
 
+#include "geo/box.h"
+
 #include "nigiri/query_generator/transport_mode.h"
 #include "nigiri/routing/clasz_mask.h"
 #include "nigiri/routing/limits.h"
 #include "nigiri/routing/location_match_mode.h"
 #include "nigiri/timetable.h"
 
+#include "transport_mode.h"
+
 namespace nigiri::query_generation {
 
 struct generator_settings {
+
+  friend std::ostream& operator<<(std::ostream& out,
+                                  generator_settings const& gs) {
+    using namespace nigiri::routing;
+    auto const match_mode_str = [](auto const& mm) {
+      return mm == location_match_mode::kIntermodal ? "intermodal" : "station";
+    };
+
+    out << "interval_size: "
+        << static_cast<double>(gs.interval_size_.count()) / 60.0 << " h";
+    if (gs.bbox_.has_value()) {
+      out << "\nbbox: min(" << gs.bbox_.value().min_ << "), max("
+          << gs.bbox_.value().max_ << ")";
+    }
+    out << "\nstart_mode: " << match_mode_str(gs.start_match_mode_)
+        << "\ndest_mode: " << match_mode_str(gs.dest_match_mode_)
+        << "\nintermodal_start_mode: " << gs.start_mode_
+        << "\nintermodal_dest_mode: " << gs.dest_mode_
+        << "\nuse_start_footpaths: "
+        << (gs.use_start_footpaths_ ? "true" : "false")
+        << "\nmax_transfers: " << std::uint32_t{gs.max_transfers_}
+        << "\nmin_connection_count: " << gs.min_connection_count_
+        << "\nextend_interval_earlier: "
+        << (gs.extend_interval_earlier_ ? "true" : "false")
+        << "\nextend_interval_later: "
+        << (gs.extend_interval_later_ ? "true" : "false")
+        << "\nprf_idx: " << std::uint32_t{gs.prf_idx_}
+        << "\nallowed_claszes: " << gs.allowed_claszes_;
+
+    auto const visit_loc = [](location_idx_t const loc_idx) {
+      std::stringstream ss;
+      ss << "station: " << loc_idx.v_;
+      return ss.str();
+    };
+    auto const visit_coord = [](geo::latlng const& coord) {
+      std::stringstream ss;
+      ss << "coordinate: " << coord;
+      return ss.str();
+    };
+    if (gs.start_.has_value()) {
+      out << "\nstart "
+          << std::visit(utl::overloaded{visit_loc, visit_coord},
+                        gs.start_.value());
+    }
+    if (gs.dest_.has_value()) {
+      out << "\ndestination "
+          << std::visit(utl::overloaded{visit_loc, visit_coord},
+                        gs.dest_.value());
+    }
+    return out;
+  }
+
   duration_t interval_size_{60U};
+  std::optional<geo::box> bbox_;
   routing::location_match_mode start_match_mode_{
       routing::location_match_mode::kIntermodal};
   routing::location_match_mode dest_match_mode_{
       routing::location_match_mode::kIntermodal};
   transport_mode start_mode_{kWalk};
   transport_mode dest_mode_{kWalk};
+  std::optional<std::variant<location_idx_t, geo::latlng>> start_;
+  std::optional<std::variant<location_idx_t, geo::latlng>> dest_;
   bool use_start_footpaths_{false};
   std::uint8_t max_transfers_{routing::kMaxTransfers};
   unsigned min_connection_count_{0U};
