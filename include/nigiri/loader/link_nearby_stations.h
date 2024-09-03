3d138d93501e1353f9518f0ef4daf346c8e8d436
mority <mority@posteo.net>
2024-09-03 12:09:53 +0200

    removes matches data structure

--- include/nigiri/loader/link_nearby_stations.h
+++ include/nigiri/loader/link_nearby_stations.h
@@ -6,7 +6,6 @@ struct timetable;
 
 namespace nigiri::loader {
 
-template <bool MatchIntraSrc, bool MatchInterSrc>
-match_set_t link_nearby_stations(timetable&);
+void link_nearby_stations(timetable&);
 
 }  // namespace nigiri::loader
