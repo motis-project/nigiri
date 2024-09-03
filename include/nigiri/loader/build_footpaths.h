f6ab32389ed339d30051d7fad446b6bfc230a557
Moritz Fischer <57353722+mority@users.noreply.github.com>
2024-09-03 16:57:33 +0200

    Remove duplicates (#128)
    * bug fix: merge trip active days, early bail out similar location sequence
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * reflexivity
    
    * intra-source merge test case
    
    * inter-source merge test case
    
    * reflexive matching
    
    * no reflexive merging
    
    * no reflexive merging
    
    * rm unused import
    
    * removes matches data structure
    
    * removes matches data structure
    
    * gets rid of templated link_nearby_stations
    
    * assert a != b when merging transports
    
    * removes matches data structure
    
    * fix var names and comments
    
    * use matches function from for_each_meta.h to compare locations for equivalence
    
    * remove matches data structure
    
    * remove matches data structure
    
    * check bitfield == 0 with none()
    
    * rm unused import
    
    * check if trip descriptor can be resolved after merge
    
    * use none() to check if bitfield == 0
    
    * moves for_each_meta.h and location_match_mode.h to top level
    
    * clang-format-17

--- include/nigiri/loader/build_footpaths.h
+++ include/nigiri/loader/build_footpaths.h
@@ -6,7 +6,8 @@ namespace nigiri::loader {
 
 void build_footpaths(timetable& tt,
                      bool adjust_footpaths,
-                     bool merge_duplicates,
+                     bool merge_dupes_intra_src,
+                     bool merge_dupes_inter_src,
                      std::uint16_t max_footpath_length);
 
 }  // namespace nigiri::loader
