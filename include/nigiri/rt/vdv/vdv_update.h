d1bd7ede3c648a24acfc7080acf43cd39f1a9b65
mority <mority@posteo.net>
2024-09-03 15:25:50 +0200

    moves implementation of updater and vdv_stop to source file

--- include/nigiri/rt/vdv/vdv_update.h
+++ include/nigiri/rt/vdv/vdv_update.h
@@ -13,49 +13,8 @@ struct timetable;
 namespace nigiri::rt::vdv {
 
 struct statistics {
-  friend std::ostream& operator<<(std::ostream& out, statistics const& s) {
-    out << "unsupported additional runs: " << s.unsupported_additional_runs_
-        << "\nunsupported cancelled runs: " << s.unsupported_cancelled_runs_
-        << "\ntotal stops: " << s.total_stops_
-        << "\nresolved stops: " << s.resolved_stops_
-        << "\nunknown stops: " << s.unknown_stops_
-        << "\nunsupported additional stops: " << s.unsupported_additional_stops_
-        << "\nno transport found at stop: " << s.no_transport_found_at_stop_
-        << "\nsearches on incomplete runs: " << s.search_on_incomplete_
-        << "\nfound runs: " << s.found_runs_
-        << "\nmultiple matches: " << s.multiple_matches_
-        << "\ntotal runs: " << s.total_runs_
-        << "\nmatched runs: " << s.matched_runs_
-        << "\nunmatchable runs: " << s.unmatchable_runs_
-        << "\nruns without stops: " << s.runs_without_stops_
-        << "\nskipped vdv stops: " << s.skipped_vdv_stops_
-        << "\nexcess vdv stops: " << s.excess_vdv_stops_
-        << "\nupdated events: " << s.updated_events_
-        << "\npropagated delays: " << s.propagated_delays_ << "\n";
-    return out;
-  }
-
-  friend statistics& operator+=(statistics& lhs, statistics const& rhs) {
-    lhs.unsupported_additional_runs_ += rhs.unsupported_additional_runs_;
-    lhs.unsupported_cancelled_runs_ += rhs.unsupported_cancelled_runs_;
-    lhs.total_stops_ += rhs.total_stops_;
-    lhs.resolved_stops_ += rhs.resolved_stops_;
-    lhs.unknown_stops_ += rhs.unknown_stops_;
-    lhs.unsupported_additional_stops_ += rhs.unsupported_additional_stops_;
-    lhs.total_runs_ += rhs.total_runs_;
-    lhs.no_transport_found_at_stop_ += rhs.no_transport_found_at_stop_;
-    lhs.search_on_incomplete_ += rhs.search_on_incomplete_;
-    lhs.found_runs_ += rhs.found_runs_;
-    lhs.multiple_matches_ += rhs.multiple_matches_;
-    lhs.matched_runs_ += rhs.matched_runs_;
-    lhs.unmatchable_runs_ += rhs.unmatchable_runs_;
-    lhs.runs_without_stops_ += rhs.runs_without_stops_;
-    lhs.skipped_vdv_stops_ += rhs.skipped_vdv_stops_;
-    lhs.excess_vdv_stops_ += rhs.excess_vdv_stops_;
-    lhs.updated_events_ += rhs.updated_events_;
-    lhs.propagated_delays_ += rhs.propagated_delays_;
-    return lhs;
-  }
+  friend std::ostream& operator<<(std::ostream&, statistics const&);
+  friend statistics& operator+=(statistics&, statistics const&);
 
   std::uint32_t unsupported_additional_runs_{0U};
   std::uint32_t unsupported_cancelled_runs_{0U};
@@ -78,14 +37,22 @@ struct statistics {
 };
 
 struct updater {
-  static constexpr auto const kAllowedTimeDiscrepancy = 10U;  // minutes
+  static constexpr auto const kExactMatchScore = 1000;
+  static constexpr auto const derive_time_discrepancy = []() {
+    auto error = 0;
+    while (kExactMatchScore - error * error > 0) {
+      ++error;
+    }
+    return error - 1;
+  };
+  static constexpr auto const kAllowedTimeDiscrepancy =
+      derive_time_discrepancy();  // minutes
 
-  updater(timetable const& tt, source_idx_t const src_idx)
-      : tt_{tt}, src_idx_{src_idx} {}
+  updater(timetable const&, source_idx_t);
 
-  void reset_vdv_run_ids_() { vdv_nigiri_.clear(); }
+  void reset_vdv_run_ids_();
 
-  statistics const& get_stats() const { return stats_; }
+  statistics const& get_stats() const;
 
   void update(rt_timetable&, pugi::xml_document const&);
 
@@ -94,26 +61,12 @@ private:
                                                 char const*);
 
   struct vdv_stop {
-    explicit vdv_stop(location_idx_t const l,
+    explicit vdv_stop(location_idx_t const,
                       std::string_view id,
-                      pugi::xml_node const n)
-        : l_{l},
-          id_{id},
-          dep_{get_opt_time(n, "Abfahrtszeit")},
-          arr_{get_opt_time(n, "Ankunftszeit")},
-          rt_dep_{get_opt_time(n, "IstAbfahrtPrognose")},
-          rt_arr_{get_opt_time(n, "IstAnkunftPrognose")} {}
+                      pugi::xml_node const);
 
     std::optional<std::pair<unixtime_t, event_type>> get_event(
-        event_type et) const {
-      if (et == event_type::kArr && arr_.has_value()) {
-        return std::pair{*arr_, event_type::kArr};
-      } else if (et == event_type::kDep && dep_.has_value()) {
-        return std::pair{*dep_, event_type::kDep};
-      } else {
-        return std::nullopt;
-      }
-    }
+        event_type et) const;
 
     location_idx_t l_;
     std::string_view id_;
--- src/rt/vdv/vdv_update.cc
+++ src/rt/vdv/vdv_update.cc
@@ -28,6 +28,57 @@ namespace nigiri::rt::vdv {
 #define vdv_trace(s)
 #endif
 
+std::ostream& operator<<(std::ostream& out, statistics const& s) {
+  out << "unsupported additional runs: " << s.unsupported_additional_runs_
+      << "\nunsupported cancelled runs: " << s.unsupported_cancelled_runs_
+      << "\ntotal stops: " << s.total_stops_
+      << "\nresolved stops: " << s.resolved_stops_
+      << "\nunknown stops: " << s.unknown_stops_
+      << "\nunsupported additional stops: " << s.unsupported_additional_stops_
+      << "\nno transport found at stop: " << s.no_transport_found_at_stop_
+      << "\nsearches on incomplete runs: " << s.search_on_incomplete_
+      << "\nfound runs: " << s.found_runs_
+      << "\nmultiple matches: " << s.multiple_matches_
+      << "\ntotal runs: " << s.total_runs_
+      << "\nmatched runs: " << s.matched_runs_
+      << "\nunmatchable runs: " << s.unmatchable_runs_
+      << "\nruns without stops: " << s.runs_without_stops_
+      << "\nskipped vdv stops: " << s.skipped_vdv_stops_
+      << "\nexcess vdv stops: " << s.excess_vdv_stops_
+      << "\nupdated events: " << s.updated_events_
+      << "\npropagated delays: " << s.propagated_delays_ << "\n";
+  return out;
+}
+
+statistics& operator+=(statistics& lhs, statistics const& rhs) {
+  lhs.unsupported_additional_runs_ += rhs.unsupported_additional_runs_;
+  lhs.unsupported_cancelled_runs_ += rhs.unsupported_cancelled_runs_;
+  lhs.total_stops_ += rhs.total_stops_;
+  lhs.resolved_stops_ += rhs.resolved_stops_;
+  lhs.unknown_stops_ += rhs.unknown_stops_;
+  lhs.unsupported_additional_stops_ += rhs.unsupported_additional_stops_;
+  lhs.total_runs_ += rhs.total_runs_;
+  lhs.no_transport_found_at_stop_ += rhs.no_transport_found_at_stop_;
+  lhs.search_on_incomplete_ += rhs.search_on_incomplete_;
+  lhs.found_runs_ += rhs.found_runs_;
+  lhs.multiple_matches_ += rhs.multiple_matches_;
+  lhs.matched_runs_ += rhs.matched_runs_;
+  lhs.unmatchable_runs_ += rhs.unmatchable_runs_;
+  lhs.runs_without_stops_ += rhs.runs_without_stops_;
+  lhs.skipped_vdv_stops_ += rhs.skipped_vdv_stops_;
+  lhs.excess_vdv_stops_ += rhs.excess_vdv_stops_;
+  lhs.updated_events_ += rhs.updated_events_;
+  lhs.propagated_delays_ += rhs.propagated_delays_;
+  return lhs;
+}
+
+updater::updater(nigiri::timetable const& tt, source_idx_t const src_idx)
+    : tt_{tt}, src_idx_{src_idx} {}
+
+void updater::reset_vdv_run_ids_() { vdv_nigiri_.clear(); }
+
+statistics const& updater::get_stats() const { return stats_; }
+
 unixtime_t parse_time(std::string const& str) {
   unixtime_t parsed;
   auto ss = std::stringstream{str};
@@ -50,6 +101,27 @@ std::optional<bool> get_opt_bool(
   auto const xpath = node.select_node(key);
   return xpath != nullptr ? utl::parse<bool>(xpath.node().child_value())
                           : default_to;
+}
+
+updater::vdv_stop::vdv_stop(nigiri::location_idx_t const l,
+                            std::string_view id,
+                            pugi::xml_node const n)
+    : l_{l},
+      id_{id},
+      dep_{get_opt_time(n, "Abfahrtszeit")},
+      arr_{get_opt_time(n, "Ankunftszeit")},
+      rt_dep_{get_opt_time(n, "IstAbfahrtPrognose")},
+      rt_arr_{get_opt_time(n, "IstAnkunftPrognose")} {}
+
+std::optional<std::pair<unixtime_t, event_type>> updater::vdv_stop::get_event(
+    event_type et) const {
+  if (et == event_type::kArr && arr_.has_value()) {
+    return std::pair{*arr_, event_type::kArr};
+  } else if (et == event_type::kDep && dep_.has_value()) {
+    return std::pair{*dep_, event_type::kDep};
+  } else {
+    return std::nullopt;
+  }
 }
 
 pugi::xml_node get(pugi::xml_node const& node, char const* str) {
@@ -112,8 +184,6 @@ struct candidate {
   std::uint32_t local_best_{0U};
   std::uint32_t total_length_;
 };
-
-constexpr auto const kExactMatchScore = 1000;
 
 std::optional<rt::run> updater::find_run(std::string_view vdv_run_id,
                                          vector<vdv_stop> const& vdv_stops,
