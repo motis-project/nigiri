eea85c72b80d523527b3b1c165c85ef5145a7f1e
mority <mority@posteo.net>
2024-09-12 17:13:07 +0200

    param name

--- src/common/parse_time.cc
+++ src/common/parse_time.cc
@@ -4,9 +4,9 @@
 
 namespace nigiri {
 
-unixtime_t parse_time(std::string const& sv) {
+unixtime_t parse_time(std::string const& str) {
   unixtime_t parsed;
-  auto ss = std::stringstream{sv};
+  auto ss = std::stringstream{str};
   ss.exceptions(std::ios::badbit | std::ios::failbit | std::ios::eofbit);
   ss >> date::parse("%FT%T", parsed);
   return parsed;
