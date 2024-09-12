e1d022d1f3df77564159fdead4aed7920637c9a5
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2024-09-12 08:25:50 +0000

    WIP: Create struct to handle shapes data

--- include/nigiri/shape.h
+++ include/nigiri/shape.h
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <array>
 #include <filesystem>
+#include <memory>
 #include <span>
 
 #include "geo/latlng.h"
@@ -12,6 +14,21 @@ struct timetable;
 }
 
 namespace nigiri {
+
+struct shapes_storage {
+    shapes_storage() = default;
+    explicit shapes_storage(
+            std::filesystem::path const&,
+            cista::mmap::protection = cista::mmap::protection::WRITE);
+    // TODO Use interval<stop_idx_t> instead
+    std::span<geo::latlng const> get_shape(timetable const&, trip_idx_t) const;
+    std::span<geo::latlng const> get_shape(timetable const&, trip_idx_t, interval<geo::latlng const> const&) const;
+    std::span<geo::latlng const> make_span(interval<geo::latlng const> const&);
+    std::unique_ptr<shapes_storage_t> data_;
+    mutable std::array<geo::latlng, 2> no_shape_cache_;
+};
+
+shape_idx_t get_shape_index(timetable const&, trip_idx_t);
 
 shapes_storage_t create_shapes_storage(
     std::filesystem::path const&,
--- src/shape.cc
+++ src/shape.cc
@@ -2,18 +2,73 @@
 
 #include "fmt/core.h"
 
+#include "geo/polyline.h"
+
 #include "nigiri/timetable.h"
 
 namespace nigiri {
 
-shapes_storage_t create_shapes_storage(std::filesystem::path const& path,
+template <typename Key, typename Value>
+mm_vecvec<Key, Value> create_storage(std::filesystem::path const& path,
                                        cista::mmap::protection const mode) {
   return {
-      cista::basic_mmap_vec<geo::latlng, std::uint64_t>{cista::mmap{
+      cista::basic_mmap_vec<Value, std::uint64_t>{cista::mmap{
           fmt::format("{}_data.bin", path.generic_string()).c_str(), mode}},
-      cista::basic_mmap_vec<cista::base_t<shape_idx_t>, std::uint64_t>{
+      cista::basic_mmap_vec<cista::base_t<Key>, std::uint64_t>{
           cista::mmap{fmt::format("{}_idx.bin", path.generic_string()).c_str(),
                       mode}}};
+}
+
+template <std::ranges::range Range>
+std::span<geo::latlng const> get_subshape(Range const shape, geo::latlng const& from, geo::latlng const& to) {
+  auto const best_from = geo::distance_to_polyline(from, shape);
+  auto const subshape_from = begin(shape) + static_cast<decltype(shape)::difference_type>(best_from.segment_idx_);
+  auto const remaining_shape = std::span{subshape_from, end(shape)};
+  auto const best_to = geo::distance_to_polyline(to, remaining_shape);
+  return {subshape_from, best_to.segment_idx_ + 1};
+}
+
+shapes_storage::shapes_storage(
+        std::filesystem::path const& path,
+        cista::mmap::protection const mode) : data_{std::make_unique<shapes_storage_t>(create_storage<shape_idx_t, geo::latlng>(path, mode))} {}
+
+std::span<geo::latlng const> shapes_storage::get_shape(timetable const& tt, trip_idx_t const trip_index) const {
+  if (data_->empty()) {
+    return {};
+  }
+  auto const shape_index = get_shape_index(tt, trip_index);
+  if (shape_index == shape_idx_t::invalid() || shape_index > data_->size()) {
+    return {};
+  }
+  auto const shape = data_->at(shape_index);
+  return {begin(shape), end(shape)};
+}
+
+std::span<geo::latlng const> shapes_storage::get_shape(timetable const& tt, trip_idx_t const trip_index, interval<geo::latlng const> const& range) const {
+  auto const shape = get_shape(tt, trip_index);
+  if (shape.empty()) {
+    return shape;
+  }
+  return get_subshape(shape, range.from_, range.to_);
+}
+
+std::span<geo::latlng const> shapes_storage::make_span(interval<geo::latlng const> const& range) {
+  no_shape_cache_ = {range.from_, range.to_};
+  return {no_shape_cache_};
+}
+
+shape_idx_t get_shape_index(timetable const& tt, trip_idx_t const trip_index) {
+  if (trip_index == trip_idx_t::invalid() || trip_index >= tt.trip_shape_indices_.size()) {
+    return shape_idx_t::invalid();
+  }
+  return tt.trip_shape_indices_.at(trip_index);
+}
+
+// ---- TODO Remove ----
+
+shapes_storage_t create_shapes_storage(std::filesystem::path const& path,
+                                       cista::mmap::protection const mode) {
+  return create_storage<shape_idx_t, geo::latlng>(path, mode);
 }
 
 std::span<geo::latlng const> get_shape(timetable const& tt,
