6bc1e7131acee05d06262c9dc22abf519a168b11
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2024-09-17 14:23:42 +0000

    Simplify cache offset calculation

--- src/loader/gtfs/shape_prepare.cc
+++ src/loader/gtfs/shape_prepare.cc
@@ -1,10 +1,15 @@
 #include "nigiri/loader/gtfs/shape_prepare.h"
 
+#include <algorithm>
+
 #include "geo/latlng.h"
 #include "geo/polyline.h"
 
+#include "utl/get_or_create.h"
 #include "utl/progress_tracker.h"
+#include "utl/zip.h"
 
+#include "nigiri/loader/gtfs/trip.h"
 #include "nigiri/shape.h"
 #include "nigiri/stop.h"
 #include "nigiri/types.h"
@@ -37,42 +42,30 @@ std::vector<location_idx_t> get_interior_locations(stop_seq_t const& stops) {
   return locations;
 }
 
-trip_idx_t find_in_cache(
-    hash_map<
-        shape_idx_t,
-        std::vector<std::pair<std::vector<location_idx_t>, trip_idx_t>>> const&
-        offsets_cache,
-    shape_idx_t shape_index,
-    std::vector<location_idx_t> const& locations) {
-  auto const cached_locations = offsets_cache.find(shape_index);
-  if (cached_locations != offsets_cache.end()) {
-    for (auto const& [known_locations, trip_index] : cached_locations->second) {
-      if (known_locations == locations) {
-        return trip_index;
-      }
-    }
-  }
-  return trip_idx_t::invalid();
-}
-
-std::vector<shape_offset_t> split_shape(
-    timetable const& tt,
-    std::span<geo::latlng const> shape,
-    std::vector<location_idx_t> const& locations) {
+std::vector<shape_offset_t> split_shape(timetable const& tt,
+                                        std::span<geo::latlng const> shape,
+                                        stop_seq_t const& stops) {
   if (shape.empty()) {
     return {};
   }
-  auto offsets = std::vector<shape_offset_t>(locations.size() + 2U);
-  offsets.front() = shape_offset_t{0};
-  offsets.back() = shape_offset_t{shape.size() - 1U};
+  auto offsets = std::vector<shape_offset_t>(stops.size());
   auto offset = shape_offset_t{0};
+
   auto index = 0U;
-  for (auto const location_index : locations) {
-    auto const location = tt.locations_.get(location_index);
-    offset += get_closest(location.pos_,
-                          shape.subspan(static_cast<std::size_t>(offset.v_)));
-    offsets[++index] = offset;
+  for (auto const& location_index : stops) {
+    if (index == 0U) {
+      offsets[0] = shape_offset_t{0};
+    } else if (index == stops.size() - 1U) {
+      offsets[index] = shape_offset_t{shape.size() - 1U};
+    } else {
+      auto const location =
+          tt.locations_.get(stop(location_index).location_idx());
+      offsets[index] = offset += get_closest(
+          location.pos_, shape.subspan(static_cast<std::size_t>(offset.v_)));
+    }
+    ++index;
   }
+
   return offsets;
 }
 
@@ -83,9 +76,30 @@ void calculate_shape_offsets(timetable const& tt,
   progress_tracker->status("Calculating shape offsets")
       .out_bounds(98.F, 100.F)
       .in_high(trips.size());
-  auto offsets_cache = hash_map<
-      shape_idx_t,
-      std::vector<std::pair<std::vector<location_idx_t>, trip_idx_t>>>{};
+  struct hash_stop_seq_t {
+    auto operator()(
+        std::pair<shape_idx_t, stop_seq_t const*> const& pair) const noexcept {
+      return cista::hashing<std::pair<shape_idx_t, stop_seq_t>>{}(
+          std::make_pair(pair.first, *pair.second));
+    }
+  };
+
+  struct equals_stop_seq_t {
+    auto operator()(
+        std::pair<shape_idx_t, stop_seq_t const*> const& lhs,
+        std::pair<shape_idx_t, stop_seq_t const*> const& rhs) const noexcept {
+      if (lhs.first != rhs.first || lhs.second->size() != rhs.second->size()) {
+        return false;
+      }
+      auto const& zip = utl::zip(*lhs.second, *rhs.second);
+      return std::all_of(begin(zip), end(zip), [](auto const pair) {
+        return std::get<0>(pair) == std::get<1>(pair);
+      });
+    }
+  };
+  auto offsets_cache =
+      hash_map<std::pair<shape_idx_t, stop_seq_t const*>, trip_idx_t,
+               hash_stop_seq_t, equals_stop_seq_t>{};
   for (auto const& trip : trips) {
     progress_tracker->update_fn();
     auto const trip_index = trip.trip_idx_;
@@ -93,24 +107,15 @@ void calculate_shape_offsets(timetable const& tt,
       shapes_data.add_offsets(trip_index, {});
       continue;
     }
-    auto const shape_index = trip.shape_idx_;
-    auto const locations = get_interior_locations(trip.stop_seq_);
-    auto const cached_trip_index =
-        find_in_cache(offsets_cache, shape_index, locations);
-    if (cached_trip_index != trip_idx_t::invalid()) {
+    auto const cached_trip_index = utl::get_or_create(
+        offsets_cache, std::make_pair(trip.shape_idx_, &trip.stop_seq_), [&]() {
+          auto const shape = shapes_data.get_shape(trip.shape_idx_);
+          auto const offsets = split_shape(tt, shape, trip.stop_seq_);
+          shapes_data.add_offsets(trip_index, offsets);
+          return trip_index;
+        });
+    if (cached_trip_index != trip_index) {
       shapes_data.duplicate_offsets(cached_trip_index, trip_index);
-    } else {
-      auto const shape = shapes_data.get_shape(shape_index);
-      auto const offsets = split_shape(tt, shape, locations);
-      shapes_data.add_offsets(trip_index, offsets);
-      auto const it = offsets_cache.find(shape_index);
-      if (it == offsets_cache.end()) {
-        offsets_cache.emplace_hint(
-            it, shape_index,
-            std::vector{std::make_pair(locations, trip_index)});
-      } else {
-        it->second.emplace_back(std::make_pair(locations, trip_index));
-      }
     }
   }
 }
