6c7802fdec1b3592da1ad1db0ac47a0b539d94a0
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2024-09-18 05:02:37 +0000

    Remove aliases used only once

--- include/nigiri/shape.h
+++ include/nigiri/shape.h
@@ -13,10 +13,7 @@ struct timetable;
 
 namespace nigiri {
 
-// using shapes_storage_t = mm_vecvec<shape_idx_t, geo::latlng>;
-
 struct shapes_storage {
-  using shape_offsets_storage_t = mm_vecvec<trip_idx_t, shape_offset_t>;
   explicit shapes_storage(
       std::filesystem::path const&,
       cista::mmap::protection = cista::mmap::protection::WRITE);
@@ -27,8 +24,8 @@ struct shapes_storage {
                                          interval<stop_idx_t> const&) const;
   void add_offsets(trip_idx_t, std::vector<shape_offset_t> const&);
   void duplicate_offsets(trip_idx_t from, trip_idx_t to);
-  shapes_storage_t data_;
-  shape_offsets_storage_t offsets_;
+  mm_vecvec<shape_idx_t, geo::latlng> data_;
+  mm_vecvec<trip_idx_t, shape_offset_t> offsets_;
 };
 
 shape_idx_t get_shape_index(timetable const&, trip_idx_t);
--- include/nigiri/types.h
+++ include/nigiri/types.h
@@ -172,7 +172,6 @@ using attribute_combination_idx_t =
     cista::strong<std::uint32_t, struct _attribute_combination>;
 using provider_idx_t = cista::strong<std::uint32_t, struct _provider_idx>;
 
-using shapes_storage_t = mm_vecvec<shape_idx_t, geo::latlng>;
 using transport_range_t = pair<transport_idx_t, interval<stop_idx_t>>;
 
 struct trip_debug {
