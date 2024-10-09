27f6c49cc17dbc27263a94d3738dc8462bd8d671
Felix Gündling <felix.guendling@gmail.com>
2024-10-09 13:37:03 +0200

    to_str(color)

--- include/nigiri/types.h
+++ include/nigiri/types.h
@@ -203,6 +203,9 @@ struct route_color {
   color_t color_;
   color_t text_color_;
 };
+inline std::string to_str(color_t const c) {
+  return fmt::format("{:06x}", to_idx(c) & 0x00ffffff);
+}
 
 struct trip_id {
   CISTA_COMPARABLE()
