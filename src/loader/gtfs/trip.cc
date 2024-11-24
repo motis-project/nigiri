85e2419ca84b855f174f5fe237166107ae6b96b0
Felix Gündling <felix.guendling@gmail.com>
2024-11-24 22:38:05 +0100

    wip

--- src/loader/gtfs/trip.cc
+++ src/loader/gtfs/trip.cc
@@ -385,9 +385,8 @@ void read_frequencies(trip_data& trips, std::string_view file_content) {
 
            // If the service operates multiple times per minute, make sure not
            // to end up with zero.
-           auto headway_minutes = duration_t{std::max(
-               int(std::lround(static_cast<float>(headway_secs) / 60)), 1)};
-
+           auto const headway_minutes = duration_t{
+               std::max(static_cast<int>(std::round(headway_secs / 60.F)), 1)};
            frequencies->emplace_back(
                frequency{hhmm_to_min(freq.start_time_->view()),
                          hhmm_to_min(freq.end_time_->view()), headway_minutes,
