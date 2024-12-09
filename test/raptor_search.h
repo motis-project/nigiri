2cf648ac22315fb6578be3f5d689f4d94e81b457
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2024-12-09 17:17:15 +0100

    Add configurable maximum travel time (#155)
    * Add option to limit maximum travel time
    
    * Revert changes to fastest_direct
    
    * Simplify runtime check for maximum travel time
    
    * Add test with invalid duration
    
    * Add tests for maximum transfers
    
    * Remove optional
    
    * Rename variable
    
    * Rename test file
    
    * Set upper limit for calculations
    
    * Simplify search interface for tests
    
    * Remove no longer used parameter
    
    * Remove unused include
    
    * Add test for edge case
    
    * Fix formatting
    
    * Improve input validation
    
    * Revert 35f64c3
    
    * Sanitize query passed to raptor_search()
    
    * Remove hardly applicable assertion
    
    * Merge sanitizer functions into single function
    
    * Fix code style

--- test/raptor_search.h
+++ test/raptor_search.h
@@ -19,8 +19,7 @@ pareto_set<routing::journey> raptor_search(
     std::string_view time,
     direction = direction::kForward,
     routing::clasz_mask_t mask = routing::all_clasz_allowed(),
-    bool require_bikes_allowed = false,
-    routing::transfer_time_settings tts = {});
+    bool require_bikes_allowed = false);
 
 pareto_set<routing::journey> raptor_search(
     timetable const&,
@@ -31,13 +30,20 @@ pareto_set<routing::journey> raptor_search(
     direction = direction::kForward,
     routing::clasz_mask_t mask = routing::all_clasz_allowed(),
     bool require_bikes_allowed = false,
-    profile_idx_t const profile = 0U,
-    routing::transfer_time_settings tts = {});
+    profile_idx_t const profile = 0U);
 
 pareto_set<routing::journey> raptor_search(timetable const&,
                                            rt_timetable const*,
                                            routing::query,
                                            direction = direction::kForward);
+
+pareto_set<routing::journey> raptor_search(timetable const& tt,
+                                           rt_timetable const* rtt,
+                                           routing::query&& q,
+                                           std::string_view from,
+                                           std::string_view to,
+                                           std::string_view time,
+                                           direction const search_dir);
 
 pareto_set<routing::journey> raptor_intermodal_search(
     timetable const&,
