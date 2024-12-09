47422f8fc5b7b61e1602dfd1cb6dcc9854f3fcc2
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2024-12-09 16:23:24 +0100

    Remove hardly applicable assertion

--- include/nigiri/routing/sanitize_query.h
+++ include/nigiri/routing/sanitize_query.h
@@ -6,10 +6,6 @@
 namespace nigiri::routing {
 
 inline void sanitize_query(query& q) {
-  // Ensure upper bound required by algorithm is a valid duration
-  constexpr auto const kAdditionalRequiredTime = decltype(kMaxTravelTime){1};
-  static_assert(kMaxTravelTime + kAdditionalRequiredTime <=
-                decltype(kMaxTravelTime)::max());
   if (q.max_travel_time_.count() < 0 || q.max_travel_time_ > kMaxTravelTime) {
     q.max_travel_time_ = kMaxTravelTime;
   }
