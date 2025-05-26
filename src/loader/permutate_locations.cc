5fb4fafda1ac279b8bbb905da012f91cef46940e
CmdrTMir <stardust.tessa@gmail.com>
2025-05-26 18:41:54 +0200

    ausgelagert in .cc Datei

---
+++ src/loader/permutate_locations.cc
@@ -0,0 +1,126 @@
+#include "permutate_locations.h"
+
+//#include "footpath.h"
+//#include "stop.h"
+//#include "types.h"
+
+//#include <algorithm>
+//#include <ranges>
+//#include <vector>
+
+namespace nigiri {
+
+vector<location_idx_t> permutation_;
+vector<location_idx_t> mapping_vec;
+
+vector<location_idx_t> create_mapping_vec(vector<location_idx_t> const& vec) {
+  auto n = vec.size();
+  std::vector<location_idx_t> result(n);
+  for (auto i = 0U; i < n; ++i) {
+    result[vec[i].v_] = location_idx_t{i};
+  }
+  vector<location_idx_t> result_vec;
+  for (auto val : result) {
+    result_vec.push_back(val);
+  }
+  return result_vec;
+}
+
+vector<location_idx_t> build_permutation_vec(
+    vecvec<location_idx_t, route_idx_t> const& order,
+    uint32_t const first_idx) {
+  permutation_.resize(order.size());
+  for (auto i = 0U; i != permutation_.size(); ++i) {
+    permutation_[i] = location_idx_t{i};
+  }
+  std::stable_sort(begin(permutation_) + first_idx, end(permutation_),
+                   [&](auto&& a, auto&& b) {
+                     return order.at(location_idx_t{a}).size() > 0 &&
+                            order.at(location_idx_t{b}).size() == 0;
+                   });
+  mapping_vec = create_mapping_vec(permutation_);
+  return permutation_;
+}
+
+template <typename T>
+T apply_permutation_vec(T const& input) {
+  T sorted;
+  for (auto i = 0U; i < input.size(); ++i) {
+    auto temp = input.at(permutation_[i]);
+    sorted.emplace_back(temp);
+  }
+  return sorted;
+}
+
+vecvec<route_idx_t, stop::value_type> apply_permutation_to_route_loc_seq(
+    vecvec<route_idx_t, stop::value_type> const& input) {
+  vecvec<route_idx_t, stop::value_type> sorted;
+  for (auto j = 0U; j < input.size(); ++j) {
+    vector<stop::value_type> new_stop_v_types;
+    auto loc_seq = input.at(route_idx_t{j});
+    for (auto s : loc_seq) {
+      auto const old_stop = stop{s};
+      auto stop_loc_idx = old_stop.location_idx();
+      auto new_idx = mapping_vec.at(stop_loc_idx.v_);
+      stop new_stop = {new_idx, old_stop.in_allowed(), old_stop.out_allowed(),
+                       old_stop.in_allowed_wheelchair(),
+                       old_stop.out_allowed_wheelchair()};
+      new_stop_v_types.emplace_back(new_stop.value());
+    }
+    sorted.emplace_back(new_stop_v_types);
+  }
+  return sorted;
+}
+
+vector_map<location_idx_t, location_idx_t> apply_permutation_and_mapping_vec(
+    vector_map<location_idx_t, location_idx_t> const& input) {
+  vector_map<location_idx_t, location_idx_t> sorted;
+  for (auto i = 0U; i < input.size(); ++i) {
+    auto temp = input.at(permutation_[i]);
+    if (temp == std::numeric_limits<uint32_t>::max()) {
+      sorted.emplace_back(temp);
+    } else {
+      sorted.emplace_back(mapping_vec.at(temp.v_));
+    }
+  }
+  return sorted;
+}
+
+mutable_fws_multimap<location_idx_t, location_idx_t> apply_permutation_multimap(
+    mutable_fws_multimap<location_idx_t, location_idx_t> const& input) {
+  mutable_fws_multimap<location_idx_t, location_idx_t> sorted;
+  for (auto i = 0U; i < input.size(); ++i) {
+    sorted.emplace_back();
+  }
+  for (auto i = 0U; i < input.size(); ++i) {
+    auto temp = input.at(permutation_[i]);
+    if (temp.size() > 0) {
+      for (auto j = 0U; j < temp.size(); ++j) {
+        sorted.emplace_back_entry(i, mapping_vec.at(temp.at(j).v_));
+      }
+    }
+  }
+  return sorted;
+}
+
+mutable_fws_multimap<location_idx_t, footpath> apply_permutation_multimap_foot(
+    mutable_fws_multimap<location_idx_t, footpath> const& input) {
+  mutable_fws_multimap<location_idx_t, footpath> sorted;
+  for (auto i = 0U; i < input.size(); ++i) {
+    sorted.emplace_back();
+  }
+  for (auto i = 0U; i < input.size(); ++i) {
+    auto temp = input.at(permutation_[i]);
+    if (temp.size() > 0) {
+      for (auto j = 0U; j < temp.size(); ++j) {
+        auto old_footpath = temp.at(j);
+        footpath new_footpath = {mapping_vec.at(old_footpath.target().v_),
+                                 old_footpath.duration()};
+        sorted.emplace_back_entry(i, new_footpath);
+      }
+    }
+  }
+  return sorted;
+}
+
+}  // namespace nigiri
