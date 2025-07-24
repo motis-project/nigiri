0ce90f41294e1ba9cc1e425f40a89f22cca709bf
Felix Gündling <felix.guendling@gmail.com>
2025-07-24 23:26:53 +0200

    pb2json / json2pb: use string_view

--- include/nigiri/rt/util.h
+++ include/nigiri/rt/util.h
@@ -4,7 +4,7 @@
 
 namespace nigiri::rt {
 
-std::string json_to_protobuf(std::string const& json);
-std::string protobuf_to_json(std::string const& protobuf);
+std::string json_to_protobuf(std::string_view);
+std::string protobuf_to_json(std::string_view);
 
 }  // namespace nigiri::rt
