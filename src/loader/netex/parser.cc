f4b78d217a4f80ed810f990248a7da8a80b10d1e
Pablo Hoch <pablo@melaxis.com>
2025-07-28 15:18:17 +0200

    netex stops: tz, crs, standlone quay, fr profile support

---
+++ src/loader/netex/parser.cc
@@ -0,0 +1,111 @@
+#include "nigiri/loader/netex/stop_places.h"
+
+#include <string_view>
+
+#include "nigiri/loader/netex/locale.h"
+#include "nigiri/loader/netex/stop_places.h"
+
+namespace nigiri::loader::netex {
+
+netex_ctx get_frame_ctx(netex_data& data,
+                        netex_ctx const& parent_ctx,
+                        pugi::xml_node const& frame) {
+  auto ctx = parent_ctx;
+
+  if (auto const valid_between = frame.child("ValidBetween")) {
+    if (auto const from_date = valid_between.child("FromDate")) {
+      ctx.valid_from_ = from_date.child_value();
+    }
+    if (auto const to_date = valid_between.child("ToDate")) {
+      ctx.valid_to_ = to_date.child_value();
+    }
+  } else if (auto const validity_conditions =
+                 frame.child("validityConditions")) {
+    // TODO: there could be multiple entries in validityConditions
+    if (auto const availability_condition =
+            validity_conditions.child("AvailabilityCondition")) {
+      if (auto const from_date = availability_condition.child("FromDate")) {
+        ctx.valid_from_ = from_date.child_value();
+      }
+      if (auto const to_date = availability_condition.child("ToDate")) {
+        ctx.valid_to_ = to_date.child_value();
+      }
+    }
+  }
+
+  if (auto const frame_defaults = frame.child("FrameDefaults")) {
+    if (auto const default_locale = frame_defaults.child("DefaultLocale")) {
+      ctx.locale_ = parse_locale(data, parent_ctx, default_locale);
+    }
+    if (auto const default_crs =
+            frame_defaults.child("DefaultLocationSystem")) {
+      ctx.default_crs_ = default_crs.child_value();
+    }
+  }
+
+  return ctx;
+}
+
+void parse_general_frame(netex_data& data,
+                         netex_ctx const& ctx,
+                         pugi::xml_node const& gen_frame) {
+  for (auto const& xpn : gen_frame.select_nodes("members/StopPlace")) {
+    handle_stop_place(data, ctx, xpn.node());
+  }
+  for (auto const& xpn : gen_frame.select_nodes("members/Quay")) {
+    handle_quay(data, ctx, xpn.node());
+  }
+}
+
+void parse_site_frame(netex_data& data,
+                      netex_ctx const& ctx,
+                      pugi::xml_node const& gen_frame) {
+  for (auto const& xpn : gen_frame.select_nodes("stopPlaces/StopPlace")) {
+    handle_stop_place(data, ctx, xpn.node());
+  }
+}
+
+void parse_composite_frame(netex_data& data,
+                           netex_ctx const& ctx,
+                           pugi::xml_node const& comp_frame) {
+  for (auto const frame : comp_frame.child("frames").children()) {
+    auto child_ctx = get_frame_ctx(data, ctx, frame);
+    switch (cista::hash(std::string_view{frame.name()})) {
+      case cista::hash("SiteFrame"):
+        parse_site_frame(data, child_ctx, frame);
+        break;
+      case cista::hash("GeneralFrame"):
+        parse_general_frame(data, child_ctx, frame);
+        break;
+      default: break;
+    }
+  }
+}
+
+void parse_netex_file(netex_data& data,
+                      loader_config const& config,
+                      source_idx_t src,
+                      timetable& tt,
+                      pugi::xml_document const& doc) {
+  auto default_locale = get_default_locale(data, config);
+  auto default_ctx = netex_ctx{
+      .locale_ = default_locale,
+  };
+
+  auto const root = doc.document_element();
+  for (auto const frame : root.child("dataObjects").children()) {
+    auto ctx = get_frame_ctx(data, default_ctx, frame);
+    switch (cista::hash(std::string_view{frame.name()})) {
+      case cista::hash("CompositeFrame"):
+        parse_composite_frame(data, ctx, frame);
+        break;
+      case cista::hash("SiteFrame"): parse_site_frame(data, ctx, frame); break;
+      case cista::hash("GeneralFrame"):
+        parse_general_frame(data, ctx, frame);
+        break;
+      default: break;
+    }
+  }
+}
+
+}  // namespace nigiri::loader::netex
