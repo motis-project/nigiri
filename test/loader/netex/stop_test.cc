7ba2d82463c675c7bc4b76c0a9c54ae9f540fd9b
Pablo Hoch <pablo@melaxis.com>
2025-08-07 17:42:12 +0200

    netex: add tests for filling in centroids

--- test/loader/netex/stop_test.cc
+++ test/loader/netex/stop_test.cc
@@ -103,6 +103,49 @@ TEST(netex, stops) {
   EXPECT_THAT(darmstadt.parent_, Eq(location_idx_t::invalid()));
   EXPECT_THAT(get_tz_name(tt, darmstadt.timezone_idx_), Eq("Europe/Berlin"));
 
+  auto const nuernberg_1_parent =
+      tt.locations_.get({"DE::StopPlace:6989_rbo_d::", {}});
+  EXPECT_THAT(nuernberg_1_parent.name_, Eq("Nürnberg"));
+  EXPECT_THAT(nuernberg_1_parent.pos_.lat_, DoubleEq(48.362493));
+  EXPECT_THAT(nuernberg_1_parent.pos_.lng_, DoubleEq(12.915564));
+  EXPECT_THAT(nuernberg_1_parent.type_, Eq(location_type::kStation));
+  EXPECT_THAT(nuernberg_1_parent.parent_, Eq(location_idx_t::invalid()));
+
+  auto const nuernberg_1_child =
+      tt.locations_.get({"DE::StopPlace:1069890000_rbo_d::", {}});
+  EXPECT_THAT(nuernberg_1_child.name_, Eq("Nürnberg"));
+  // this stop doesn't have a centroid in the netex data
+  // should be calculated as the centroid of its bounding box (from the quays)
+  EXPECT_THAT(nuernberg_1_child.pos_.lat_, DoubleNear(48.362493, 0.00001));
+  EXPECT_THAT(nuernberg_1_child.pos_.lng_, DoubleNear(12.915564, 0.00001));
+  EXPECT_THAT(nuernberg_1_child.type_, Eq(location_type::kStation));
+  EXPECT_THAT(nuernberg_1_child.parent_, Eq(nuernberg_1_parent.l_));
+  auto const nuernberg_1_quay_1 =
+      tt.locations_.get({"DE::Quay:10698902_rbo_d::", {}});
+  EXPECT_THAT(nuernberg_1_quay_1.pos_.lat_, DoubleEq(48.362451));
+  EXPECT_THAT(nuernberg_1_quay_1.pos_.lng_, DoubleEq(12.915393));
+  auto const nuernberg_1_quay_2 =
+      tt.locations_.get({"DE::Quay:10698901_rbo_d::", {}});
+  EXPECT_THAT(nuernberg_1_quay_2.pos_.lat_, DoubleEq(48.362535));
+  EXPECT_THAT(nuernberg_1_quay_2.pos_.lng_, DoubleEq(12.915735));
+
+  auto airport_business_park =
+      tt.locations_.get({"DE::StopPlace:4113_GVH::", {}});
+  EXPECT_THAT(airport_business_park.name_, Eq("Airport Business Park West"));
+  EXPECT_THAT(airport_business_park.pos_.lat_, DoubleEq(52.461497));
+  EXPECT_THAT(airport_business_park.pos_.lng_, DoubleEq(9.680919));
+  EXPECT_THAT(airport_business_park.type_, Eq(location_type::kStation));
+  // this quay doesn't have a centroid in the netex data
+  // should be loaded with the centroid of the stop place
+  auto const airport_business_park_quay =
+      tt.locations_.get({"DE::Quay:1041130001_GVH::", {}});
+  EXPECT_THAT(airport_business_park_quay.type_, Eq(location_type::kTrack));
+  EXPECT_THAT(airport_business_park_quay.parent_, Eq(airport_business_park.l_));
+  EXPECT_THAT(airport_business_park_quay.pos_.lat_,
+              DoubleEq(airport_business_park.pos_.lat_));
+  EXPECT_THAT(airport_business_park_quay.pos_.lng_,
+              DoubleEq(airport_business_park.pos_.lng_));
+
   // NO
 
   auto const oslo_s_parent = tt.locations_.get({"NSR:StopPlace:59872", {}});
