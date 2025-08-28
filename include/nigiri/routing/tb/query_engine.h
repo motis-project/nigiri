abc8341da3a7c93361b747e1980469d666584ac0
Felix Gündling <felix.guendling@gmail.com>
2025-08-28 15:51:27 +0200

    wip

--- include/nigiri/routing/tb/query_engine.h
+++ include/nigiri/routing/tb/query_engine.h
@@ -1,38 +1,23 @@
 #pragma once
 
-#include <cinttypes>
-
 #include "nigiri/routing/journey.h"
 #include "nigiri/routing/pareto_set.h"
 #include "nigiri/routing/query.h"
-#include "nigiri/routing/tb/preprocess.h"
 #include "nigiri/routing/tb/queue.h"
 #include "nigiri/routing/tb/reached.h"
 #include "nigiri/routing/tb/settings.h"
 
 namespace nigiri {
 struct timetable;
-}  // namespace nigiri
+}
 
 namespace nigiri::routing::tb {
 
 struct queue_entry;
 
-// a route that reaches the destination
-struct route_dest {
-  route_dest(std::uint16_t stop_idx, std::uint16_t time)
-      : stop_idx_(stop_idx), time_(time) {}
-  // the stop index at which the route reaches the target location
-  std::uint16_t stop_idx_;
-  // the time in it takes after exiting the route until the target location is
-  // reached
-  std::uint16_t time_;
-};
-
 struct query_state {
-  query_state(timetable const& tt, tb_data const& tbd)
-      : tbd_{tbd}, r_{tt}, q_n_{r_} {
-    t_min_.resize(kNumTransfersMax, unixtime_t::max());
+  query_state(timetable const& tt, tb_data const& tbd) : tbd_{tbd}, r_{tt} {
+    t_min_.fill(unixtime_t::max());
     q_n_.q_.reserve(10'000'000);
     end_reachable_.resize(tbd.segment_transfers_.size());
   }
@@ -44,18 +29,14 @@ struct query_state {
     end_reachable_.zero_out();
   }
 
-  // transfer set built by preprocessor
   tb_data const& tbd_;
 
-  // reached stops per transport
   reached r_;
+  queue q_n_{r_};
 
-  // minimum arrival times per number of transfers
-  std::vector<unixtime_t> t_min_;
-  std::vector<queue_idx_t> parent_;
-
-  // queues of transport segments
-  queue q_n_;
+  // minimum arrival times per round
+  std::array<unixtime_t, kMaxTransfers + 1U> t_min_;
+  std::array<queue_idx_t, kMaxTransfers + 1U> parent_;
 
   bitvec_map<segment_idx_t> end_reachable_;
   hash_map<segment_idx_t, duration_t> dist_to_dest_;
@@ -80,22 +61,21 @@ struct query_engine {
   static constexpr auto const kUnreachable =
       std::numeric_limits<std::uint16_t>::max();
 
-  query_engine(
-      timetable const& tt,
-      rt_timetable const* rtt,
-      query_state& state,
-      bitvec& is_dest,
-      std::optional<std::array<bitvec, kMaxVias>> is_via,  // unsupported
-      std::vector<std::uint16_t>& dist_to_dest,
-      std::optional<hash_map<location_idx_t, std::vector<td_offset>>>
-          td_dist_to_dest,  // unsupported
-      std::vector<std::uint16_t>& lb,
-      std::optional<std::vector<via_stop>> via_stops,  // unsupported
-      day_idx_t const base,
-      std::optional<clasz_mask_t> allowed_claszes,  // unsupported
-      std::optional<bool> require_bike_transport,  // unsupported
-      std::optional<bool> is_wheelchair,  // unsupported
-      std::optional<transfer_time_settings> tts);  // unsupported
+  query_engine(timetable const&,
+               rt_timetable const*,
+               query_state&,
+               bitvec& is_dest,
+               std::array<bitvec, kMaxVias>&,
+               std::vector<std::uint16_t>& dist_to_dest,
+               hash_map<location_idx_t, std::vector<td_offset>> const&,
+               std::vector<std::uint16_t>& lb,
+               std::vector<via_stop> const&,
+               day_idx_t const base,
+               clasz_mask_t,
+               bool,
+               bool,
+               bool,
+               transfer_time_settings);
 
   algo_stats_t get_stats() const { return stats_; }
 
@@ -120,55 +100,11 @@ struct query_engine {
   void reconstruct(query const& q, journey& j) const;
 
 private:
-  void handle_start_footpath(day_idx_t,
-                             minutes_after_midnight_t,
-                             footpath const);
-
   void seg_dest(std::uint8_t k, queue_idx_t);
   void seg_prune(std::uint8_t k, queue_entry&);
   void seg_transfers(std::uint8_t k, queue_idx_t);
 
-  struct journey_end {
-    journey_end(queue_idx_t const seg_idx,
-                route_dest const& le,
-                location_idx_t const le_location,
-                location_idx_t const last_location)
-        : seg_idx_(seg_idx),
-          le_(le),
-          le_location_(le_location),
-          last_location_(last_location) {}
-
-    // the last transport segment of the journey
-    queue_idx_t seg_idx_;
-    // the l_entry for the destination of the journey
-    route_dest le_;
-    // the location idx of the l_entry
-    location_idx_t le_location_;
-    // the reconstructed destination of the journey
-    location_idx_t last_location_;
-  };
-
-  std::optional<journey_end> reconstruct_journey_end(query const& q,
-                                                     journey const& j) const;
-
-  void add_final_footpath(query const& q,
-                          journey& j,
-                          journey_end const& je) const;
-
-  void add_segment_leg(journey& j, queue_entry const& seg) const;
-
-  // reconstruct the transfer from the given segment to the last journey leg
-  // returns the stop idx at which the segment is exited
-  std::optional<queue_entry> reconstruct_transfer(journey& j,
-                                                  queue_entry const& seg_next,
-                                                  std::uint8_t n) const;
-
-  void add_initial_footpath(query const& q, journey& j) const;
-
-  bool is_start_location(query const&, location_idx_t const) const;
-
   timetable const& tt_;
-  rt_timetable const* rtt_;
   query_state& state_;
   bitvec& is_dest_;
   std::vector<std::uint16_t>& dist_to_dest_;
