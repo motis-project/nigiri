3e403d41679127a176aba64873915d904c7888c9
Jonas-July <jonas.july@outlook.de>
2025-09-04 12:38:05 +0200

    feat: Use cached timetables to improve import time
    Since the intermediate data necessary for caching is stored, we
    can use it to load from a previous checkpoint when restarting an import.
    
    To find out which sources we can skip, we load the previous change
    detector and compare it with the current one. Due to the linear
    dependency of importing, we can just check that each source in the list
    is unchanged in the order they are presented.
    The source arrays must be the same size, otherwise the caches may
    overwrite each other, making them invalid.
    If a changed source is found (path and/or last write time don't match),
    all successive sources will also need recomputing, thus break.
    
    In order to restart from cache, we need to load the cached result.
    Due to linearity, again, we can just load the result of the previous
    source which hasn't changed and thus would produce equivalent
    timetable and shape data.
    There could still be issues though. If the cached timetable cannot be
    loaded for some reason, we can't use it and have to recompute it
    anyway. The same is true if the date_range doesn't match. In that case,
    the then previous one has to be loaded (and the one before that, etc.).
    If the very first source has to be recomputed, there is no cache left,
    and the initial timetable with the special stations is created and
    provided.
    
    The timetable is always finalised at the end, so changing finalize_opt
    don't have to be considered. If the assistance_times change, the cache
    may be invalid and has to be deleted. The code doesn't check for that.
    
    It's probably possible to reorder the input sources to potentially get
    more in common with the cached state, but that would make equivalence
    a bit harder to prove. The order usually doesn't change anyway or at
    least it can be prevented on the caller side.

--- src/loader/load.cc
+++ src/loader/load.cc
@@ -69,6 +69,78 @@ timetable load(std::vector<timetable_source> const& sources,
         cista::hashing<loader_config>{}(local_config));
     chg.last_write_times_.emplace_back(last_write_time_t{timestamp});
   }
+  auto saved_changes = change_detector{};
+  try {
+    saved_changes = *cista::read<change_detector>(cache_metadata_path);
+  } catch (std::exception const& e) {
+    log(log_lvl::info, "loader.load", "no cache metadata at {} found",
+        cache_metadata_path);
+  }
+  auto first_recomputed_source = source_idx_t{chg.source_paths_.size()};
+  for (auto const [idx, path] : utl::enumerate(chg.source_paths_)) {
+    auto const src = source_idx_t{idx};
+    if (chg.source_paths_.size() != saved_changes.source_paths_.size()) {
+      first_recomputed_source = src;
+      break;
+    }
+    if (path != saved_changes.source_paths_[src] ||
+        chg.last_write_times_[src] != saved_changes.last_write_times_[src]) {
+      first_recomputed_source = src;
+      break;
+    }
+    if (chg.source_config_hashes_[src] !=
+        saved_changes.source_config_hashes_[src]) {
+      first_recomputed_source = src;
+      break;
+    }
+  }
+
+  auto tt = timetable{};
+  auto const progress_tracker = utl::get_active_progress_tracker();
+  for (auto i = first_recomputed_source; i >= 0; --i) {
+    if (i == 0) {
+      tt.date_range_ = date_range;
+      tt.n_sources_ = static_cast<cista::base_t<source_idx_t>>(sources.size());
+      register_special_stations(tt);
+      break;
+    }
+    auto const prev = i - 1;
+    auto const local_cache_path =
+        cache_path / fmt::format("tt{:d}", to_idx(prev));
+    auto cached_timetable = timetable{};
+    try {
+      cached_timetable = *cista::read<timetable>(local_cache_path / "tt.bin");
+    } catch (std::exception const& e) {
+      log(log_lvl::info, "loader.load", "no cached timetable at {} found",
+          local_cache_path / "tt.bin");
+      continue;
+    }
+    cached_timetable.resolve();
+    if (cached_timetable.date_range_ != date_range) {
+      continue;
+    }
+    first_recomputed_source = i;
+    tt = cached_timetable;
+    auto cached_shape_store = std::make_unique<shapes_storage>(
+        local_cache_path, cista::mmap::protection::READ);
+    for (auto i = 0U; i < cached_shape_store->data_.size(); ++i) {
+      auto const idx = shape_idx_t{i};
+      shapes->data_.emplace_back(cached_shape_store->data_[idx]);
+    }
+    for (auto e : cached_shape_store->offsets_) {
+      shapes->offsets_.emplace_back(e);
+    }
+    for (auto e : cached_shape_store->trip_offset_indices_) {
+      shapes->trip_offset_indices_.emplace_back(e);
+    }
+    for (auto e : cached_shape_store->route_bboxes_) {
+      shapes->route_bboxes_.emplace_back(e);
+    }
+    for (auto e : cached_shape_store->route_segment_bboxes_) {
+      shapes->route_segment_bboxes_.emplace_back(e);
+    }
+    break;
+  }
 
   try {
     cista::write(cache_metadata_path, chg);
@@ -77,16 +149,14 @@ timetable load(std::vector<timetable_source> const& sources,
         cache_metadata_path);
   }
 
-  auto tt = timetable{};
-  tt.date_range_ = date_range;
-  tt.n_sources_ = static_cast<cista::base_t<source_idx_t>>(sources.size());
-  register_special_stations(tt);
-  auto const progress_tracker = utl::get_active_progress_tracker();
   for (auto const [idx, in] : utl::enumerate(sources)) {
     auto const local_cache_path = cache_path / fmt::format("tt{:d}", idx);
+    auto const src = source_idx_t{idx};
+    if (src < first_recomputed_source) {
+      continue;
+    }
     auto const& [tag, path, local_config] = in;
     auto const is_in_memory = path.starts_with("\n#");
-    auto const src = source_idx_t{idx};
     auto const dir = is_in_memory
                          // hack to load strings in integration tests
                          ? std::make_unique<mem_dir>(mem_dir::read(path))
