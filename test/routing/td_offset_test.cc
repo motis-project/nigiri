31496159a454851d637ef419577dc6345c3e81dc
mority <mority@posteo.net>
2025-11-08 11:27:39 +0100

    1 min validity tests

--- test/routing/td_offset_test.cc
+++ test/routing/td_offset_test.cc
@@ -49,7 +49,7 @@ T1,11:00:00,11:00:00,B,2,0,0
 
 }  // namespace
 
-constexpr auto const kExpStartFwd = R"(
+constexpr auto kExpStartFwd = R"(
 [2024-06-19 07:00, 2024-06-19 09:00]
 TRANSFERS: 0
      FROM: (START, START) [2024-06-19 07:45]
@@ -99,7 +99,7 @@ TEST(routing, td_start_fwd) {
   EXPECT_EQ(kExpStartFwd, to_string(tt, run_search()));
 }
 
-constexpr auto const kExpDestFwd = R"(
+constexpr auto kExpDestFwd = R"(
 [2024-06-19 07:00, 2024-06-19 09:15]
 TRANSFERS: 0
      FROM: (START, START) [2024-06-19 07:00]
@@ -149,7 +149,7 @@ TEST(routing, td_dest_fwd) {
   EXPECT_EQ(kExpDestFwd, to_string(tt, run_search()));
 }
 
-constexpr auto const kExpStartBwd [[maybe_unused]] = R"(
+constexpr auto kExpStartBwd = R"(
 [2024-06-19 08:00, 2024-06-19 10:00]
 TRANSFERS: 0
      FROM: (END, END) [2024-06-19 08:00]
@@ -199,7 +199,7 @@ TEST(routing, td_start_bwd) {
   EXPECT_EQ(kExpStartBwd, to_string(tt, run_search()));
 }
 
-constexpr auto const kExpDestBwd = R"(
+constexpr auto kExpDestBwd = R"(
 [2024-06-19 07:45, 2024-06-19 10:00]
 TRANSFERS: 0
      FROM: (END, END) [2024-06-19 07:45]
@@ -249,12 +249,12 @@ TEST(routing, td_dest_bwd) {
   EXPECT_EQ(kExpDestBwd, to_string(tt, run_search()));
 }
 
-constexpr auto const kExpStartFwd1minValidity = R"(
+constexpr auto kExpFirstMileFwd1minValidity = R"(
 [2024-06-19 07:30, 2024-06-19 09:00]
 TRANSFERS: 0
      FROM: (START, START) [2024-06-19 07:30]
        TO: (END, END) [2024-06-19 09:00]
-leg 0: (START, START) [2024-06-19 07:30] -> (A, A) [2024-06-19 07:30]
+leg 0: (START, START) [2024-06-19 07:30] -> (A, A) [2024-06-19 07:40]
   MUMO (id=5, duration=10)
 leg 1: (A, A) [2024-06-19 08:00] -> (B, B) [2024-06-19 09:00]
    0: A       A...............................................                               d: 19.06 08:00 [19.06 10:00]  [{name=RE 1, day=2024-06-19, id=T1, src=0}]
@@ -264,7 +264,7 @@ leg 2: (B, B) [2024-06-19 09:00] -> (END, END) [2024-06-19 09:00]
 
 )";
 
-TEST(routing, td_start_fwd_1min_validity) {
+TEST(routing, first_mile_fwd_1min_validity) {
   timetable tt;
   tt.date_range_ = {date::sys_days{2024_y / June / 18},
                     date::sys_days{2024_y / June / 20}};
@@ -279,9 +279,8 @@ TEST(routing, td_start_fwd_1min_validity) {
     return raptor_search(
         tt, nullptr,
         routing::query{
-            .start_time_ =
-                interval<unixtime_t>{{sys_days{2024_y / June / 19} + 7h},
-                                     {sys_days{2024_y / June / 19} + 9h}},
+            .start_time_ = interval<unixtime_t>{{sys_days{2024_y / June / 19}},
+                                                {sys_days{2024_y / June / 20}}},
             .start_match_mode_ = routing::location_match_mode::kIntermodal,
             .dest_match_mode_ = routing::location_match_mode::kIntermodal,
             .use_start_footpaths_ = false,
@@ -303,5 +302,173 @@ TEST(routing, td_start_fwd_1min_validity) {
 
   std::cout << "\n" << to_string(tt, run_search()) << "\n";
 
-  EXPECT_EQ(kExpStartFwd1minValidity, to_string(tt, run_search()));
+  EXPECT_EQ(kExpFirstMileFwd1minValidity, to_string(tt, run_search()));
+}
+
+constexpr auto kExpFirstMileBwd1minValidity = R"(
+[2024-06-19 07:30, 2024-06-19 09:00]
+TRANSFERS: 0
+     FROM: (END, END) [2024-06-19 07:30]
+       TO: (START, START) [2024-06-19 09:00]
+leg 0: (END, END) [2024-06-19 07:30] -> (A, A) [2024-06-19 07:40]
+  MUMO (id=5, duration=10)
+leg 1: (A, A) [2024-06-19 08:00] -> (B, B) [2024-06-19 09:00]
+   0: A       A...............................................                               d: 19.06 08:00 [19.06 10:00]  [{name=RE 1, day=2024-06-19, id=T1, src=0}]
+   1: B       B............................................... a: 19.06 09:00 [19.06 11:00]
+leg 2: (B, B) [2024-06-19 09:00] -> (START, START) [2024-06-19 09:00]
+  MUMO (id=0, duration=0)
+
+)";
+
+TEST(routing, first_mile_bwd_1min_validity) {
+  timetable tt;
+  tt.date_range_ = {date::sys_days{2024_y / June / 18},
+                    date::sys_days{2024_y / June / 20}};
+  register_special_stations(tt);
+  load_timetable({}, source_idx_t{0}, test_files(), tt);
+  finalize(tt);
+
+  auto const A = tt.locations_.get({"A", {}}).l_;
+  auto const B = tt.locations_.get({"B", {}}).l_;
+
+  auto const run_search = [&]() {
+    return raptor_search(
+        tt, nullptr,
+        routing::query{
+            .start_time_ = interval<unixtime_t>{{sys_days{2024_y / June / 19}},
+                                                {sys_days{2024_y / June / 20}}},
+            .start_match_mode_ = routing::location_match_mode::kIntermodal,
+            .dest_match_mode_ = routing::location_match_mode::kIntermodal,
+            .use_start_footpaths_ = false,
+            .start_ = {{B, duration_t{0}, transport_mode_id_t{0}}},
+            .td_dest_ =
+                {{{A,
+                   {{.valid_from_ = unixtime_t{0h},
+                     .duration_ = footpath::kMaxDuration,
+                     .transport_mode_id_ = 5},
+                    {.valid_from_ = sys_days{2024_y / June / 19} + 7h + 30min,
+                     .duration_ = 10min,
+                     .transport_mode_id_ = 5},
+                    {.valid_from_ = sys_days{2024_y / June / 19} + 7h + 31min,
+                     .duration_ = footpath::kMaxDuration,
+                     .transport_mode_id_ = 5}}}}},
+            .prf_idx_ = 0U},
+        direction::kBackward);
+  };
+
+  std::cout << "\n" << to_string(tt, run_search()) << "\n";
+
+  EXPECT_EQ(kExpFirstMileBwd1minValidity, to_string(tt, run_search()));
+}
+
+constexpr auto kExpLastMileFwd1minValidity = R"(
+[2024-06-19 08:00, 2024-06-19 09:40]
+TRANSFERS: 0
+     FROM: (START, START) [2024-06-19 08:00]
+       TO: (END, END) [2024-06-19 09:40]
+leg 0: (START, START) [2024-06-19 08:00] -> (A, A) [2024-06-19 08:00]
+  MUMO (id=0, duration=0)
+leg 1: (A, A) [2024-06-19 08:00] -> (B, B) [2024-06-19 09:00]
+   0: A       A...............................................                               d: 19.06 08:00 [19.06 10:00]  [{name=RE 1, day=2024-06-19, id=T1, src=0}]
+   1: B       B............................................... a: 19.06 09:00 [19.06 11:00]
+leg 2: (B, B) [2024-06-19 09:30] -> (END, END) [2024-06-19 09:40]
+  MUMO (id=5, duration=10)
+
+)";
+
+TEST(routing, last_mile_fwd_1min_validity) {
+  timetable tt;
+  tt.date_range_ = {date::sys_days{2024_y / June / 18},
+                    date::sys_days{2024_y / June / 20}};
+  register_special_stations(tt);
+  load_timetable({}, source_idx_t{0}, test_files(), tt);
+  finalize(tt);
+
+  auto const A = tt.locations_.get({"A", {}}).l_;
+  auto const B = tt.locations_.get({"B", {}}).l_;
+
+  auto const run_search = [&]() {
+    return raptor_search(
+        tt, nullptr,
+        routing::query{
+            .start_time_ = interval<unixtime_t>{{sys_days{2024_y / June / 19}},
+                                                {sys_days{2024_y / June / 20}}},
+            .start_match_mode_ = routing::location_match_mode::kIntermodal,
+            .dest_match_mode_ = routing::location_match_mode::kIntermodal,
+            .use_start_footpaths_ = false,
+            .start_ = {{A, duration_t{0}, transport_mode_id_t{0}}},
+            .td_dest_ =
+                {{{B,
+                   {{.valid_from_ = unixtime_t{0h},
+                     .duration_ = footpath::kMaxDuration,
+                     .transport_mode_id_ = 5},
+                    {.valid_from_ = sys_days{2024_y / June / 19} + 9h + 30min,
+                     .duration_ = 10min,
+                     .transport_mode_id_ = 5},
+                    {.valid_from_ = sys_days{2024_y / June / 19} + 9h + 31min,
+                     .duration_ = footpath::kMaxDuration,
+                     .transport_mode_id_ = 5}}}}},
+            .prf_idx_ = 0U},
+        direction::kForward);
+  };
+
+  std::cout << "\n" << to_string(tt, run_search()) << "\n";
+
+  EXPECT_EQ(kExpLastMileFwd1minValidity, to_string(tt, run_search()));
+}
+
+constexpr auto kExpLastMileBwd1minValidity = R"(
+[2024-06-19 08:00, 2024-06-19 09:40]
+TRANSFERS: 0
+     FROM: (END, END) [2024-06-19 08:00]
+       TO: (START, START) [2024-06-19 09:40]
+leg 0: (END, END) [2024-06-19 08:00] -> (A, A) [2024-06-19 08:00]
+  MUMO (id=0, duration=0)
+leg 1: (A, A) [2024-06-19 08:00] -> (B, B) [2024-06-19 09:00]
+   0: A       A...............................................                               d: 19.06 08:00 [19.06 10:00]  [{name=RE 1, day=2024-06-19, id=T1, src=0}]
+   1: B       B............................................... a: 19.06 09:00 [19.06 11:00]
+leg 2: (B, B) [2024-06-19 09:30] -> (START, START) [2024-06-19 09:40]
+  MUMO (id=5, duration=10)
+
+)";
+
+TEST(routing, last_mile_bwd_1min_validity) {
+  timetable tt;
+  tt.date_range_ = {date::sys_days{2024_y / June / 18},
+                    date::sys_days{2024_y / June / 20}};
+  register_special_stations(tt);
+  load_timetable({}, source_idx_t{0}, test_files(), tt);
+  finalize(tt);
+
+  auto const A = tt.locations_.get({"A", {}}).l_;
+  auto const B = tt.locations_.get({"B", {}}).l_;
+
+  auto const run_search = [&]() {
+    return raptor_search(
+        tt, nullptr,
+        routing::query{
+            .start_time_ = interval<unixtime_t>{{sys_days{2024_y / June / 19}},
+                                                {sys_days{2024_y / June / 19}}},
+            .start_match_mode_ = routing::location_match_mode::kIntermodal,
+            .dest_match_mode_ = routing::location_match_mode::kIntermodal,
+            .use_start_footpaths_ = false,
+            .destination_ = {{A, duration_t{0}, transport_mode_id_t{0}}},
+            .td_start_ =
+                {{{B,
+                   {{.valid_from_ = unixtime_t{0h},
+                     .duration_ = footpath::kMaxDuration,
+                     .transport_mode_id_ = 5},
+                    {.valid_from_ = sys_days{2024_y / June / 19} + 9h + 30min,
+                     .duration_ = 10min,
+                     .transport_mode_id_ = 5},
+                    {.valid_from_ = sys_days{2024_y / June / 19} + 9h + 31min,
+                     .duration_ = footpath::kMaxDuration,
+                     .transport_mode_id_ = 5}}}}},
+            .prf_idx_ = 0U},
+        direction::kForward);
+  };
+
+  std::cout << "\n" << to_string(tt, run_search()) << "\n";
+
+  EXPECT_EQ(kExpLastMileBwd1minValidity, to_string(tt, run_search()));
 }
