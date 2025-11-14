b2bbeb919c2f5b413de019f94bb4aa5e0d4f1a91
Jonas-July <jonas.july@outlook.de>
2025-11-14 16:16:08 +0100

    feat(load.cc): Load general tree-like cache structures
    The `loading_tree` structure manages the binary cache tree. This
    design allows arbitrary binary trees to be loaded from cache. The
    loading is done via recursion over both children.
    
    The nodes are referenced via indices and their relationships are
    described by the parents_, left_, and right_ vectors.
    The first node (idx=0) is always the default timetable. After that
    follow the other source based nodes, and then the intermediate merging
    nodes. In total, there are always 2(n+1)-1 nodes, where n is the number
    of timetable sources.
    
    Each node has a corresponding cache location to which it will save its
    result. By default, it is the node index, but that can be changed if
    necessary. To avoid issues between the automatic and the manual indices,
    the manual indices can't be set to an automatic value.
    
    Building up the tree has 5 steps:
     - Insert the default node at index 0
     - Insert source nodes up to n+1
     - Determine the updated sources by comparing with the previous cache
     - Insert intermediate nodes. They are updated if either child is updated
     - Write the cache structure to disk. From now on, it is constant.
    
    Loading from the tree-like cache is done via tree traversal. There are
    3 types of nodes:
     - The default timetable node at index 0,
     - the loading nodes up to l.source_paths_.size(),
     - the merging nodes after that.
    
    1. The default timetable gets constructed on the fly.
    2. The latter two will check whether they need to be recomputed. If they
       don't, they'll try to read the timetable from the cache.
    3. If the node needs to be recomputed (either due to update or cache
       failure), we need to determine which kind of node it is:
    3.1. Merging nodes call both children recursively. If that fails and they
         return nothing, then they get skipped. If both have been loaded
         successfully, their timetables are merged and written. The shapes
         only get saved if both are available; Usually either both are or both
         are not, as that is dependent on the global setting of whether to
         save the shapes or not. If they get saved, the left gets added first,
         then the right one.
    3.2. If it is a source based node, then it will try to load from source.
         On success, it writes the resulting timetable into the cache
         location; The shapes are automatically written to there during the
         loading from source.
         In case the loading fails, this is propagated upwards, where it
         can be skipped.
    
    Finally, the timetable gets finalized. This is unchanged.

--- src/loader/load.cc
+++ src/loader/load.cc
@@ -1,5 +1,5 @@
+#include <cassert>
 #include <chrono>
-#include <iterator>
 
 #include "nigiri/loader/load.h"
 
@@ -119,12 +119,157 @@ std::optional<load_result> load_from_cache(
 
 using last_write_time_t = cista::strong<std::int64_t, struct _last_write_time>;
 using source_path_t = cista::basic_string<char const*>;
+using node_idx_t = cista::strong<std::uint32_t, struct _node_idx>;
 
-struct change_detector {
+struct loading_tree {
+  node_idx_t n_nodes() const {
+    return parent_.size() > 0 ? node_idx_t{2 * parent_.size() - 1}
+                              : node_idx_t{0};
+  }
+
+  node_idx_t leaf_index(source_idx_t const& src) const {
+    // Index 0 is the default timetable
+    // After that, the source nodes follow
+    // Then all intermediate nodes
+    return node_idx_t{cista::to_idx(src) + 1};
+  }
+
+  node_idx_t insert_node(uint64_t cache_idx = 0) {
+    return insert_node(node_idx_t::invalid(), node_idx_t::invalid(), cache_idx);
+  }
+
+  node_idx_t insert_node(node_idx_t left,
+                         node_idx_t right,
+                         uint64_t cache_idx = 0,
+                         node_idx_t parent = node_idx_t::invalid()) {
+    auto node_idx = node_idx_t{parent_.size()};
+
+    parent_.emplace_back(parent);
+    left_.emplace_back(left);
+    right_.emplace_back(right);
+
+    // Avoid accidental duplication
+    cache_idx = cache_idx < cista::to_idx(n_nodes()) ? cista::to_idx(node_idx)
+                                                     : cache_idx;
+    cache_indices_.emplace_back(cache_idx);
+
+    if (parent != node_idx_t::invalid()) {
+      if (left_[parent] == node_idx_t::invalid()) {
+        left_[parent] = node_idx;
+      } else if (right_[parent] == node_idx_t::invalid()) {
+        right_[parent] = node_idx;
+      } else {
+        throw utl::fail("invalid parent {} for {}", parent, node_idx);
+      }
+    }
+    if (left != node_idx_t::invalid()) {
+      if (parent_[left] == node_idx_t::invalid()) {
+        parent_[left] = node_idx;
+      } else {
+        throw utl::fail("invalid left child {} for {}", left, node_idx);
+      }
+    }
+    if (right != node_idx_t::invalid()) {
+      if (parent_[right] == node_idx_t::invalid()) {
+        parent_[right] = node_idx;
+      } else {
+        throw utl::fail("invalid right child {} for {}", right, node_idx);
+      }
+    }
+    return node_idx;
+  }
+
   vector_map<source_idx_t, source_path_t> source_paths_;
   vector_map<source_idx_t, std::uint64_t> source_config_hashes_;
   vector_map<source_idx_t, last_write_time_t> last_write_times_;
+
+  vector_map<node_idx_t, node_idx_t> parent_;
+  vector_map<node_idx_t, node_idx_t> left_;
+  vector_map<node_idx_t, node_idx_t> right_;
+
+  vector_map<node_idx_t, uint64_t> cache_indices_;
 };
+
+std::optional<load_result> run_loading(
+    loading_tree const& l,
+    node_idx_t const root,
+    bitvec_map<node_idx_t> const& needs_recomputation,
+    vector_map<source_idx_t, loading_source_node> const& source_loading_nodes,
+    fs::path const& cache_path,
+    interval<date::sys_days> const& date_range,
+    auto& progress_tracker) {
+  auto const local_cache_path =
+      cache_path / fmt::format("tt{:d}", l.cache_indices_[root]);
+  if (root == node_idx_t{0}) {
+    auto tt = timetable{};
+    tt.date_range_ = date_range;
+    tt.n_sources_ = 0U;
+    register_special_stations(tt);
+    auto shape_store = std::make_unique<shapes_storage>(
+        local_cache_path, cista::mmap::protection::WRITE);
+    return load_result{.tt_ = tt, .shapes_ = std::move(shape_store)};
+  }
+  if (!needs_recomputation[root]) {
+    auto result = load_from_cache(local_cache_path, date_range);
+    if (result.has_value()) {
+      return result;
+    }
+  }
+  fs::create_directories(local_cache_path);
+  auto const is_merge_node = cista::to_idx(root) > l.source_paths_.size();
+  if (is_merge_node) {
+    auto const left = l.left_[root];
+    auto const right = l.right_[root];
+    auto left_result =
+        run_loading(l, left, needs_recomputation, source_loading_nodes,
+                    cache_path, date_range, progress_tracker);
+    auto right_result =
+        run_loading(l, right, needs_recomputation, source_loading_nodes,
+                    cache_path, date_range, progress_tracker);
+    if (!left_result.has_value()) {
+      return right_result;
+    } else if (!right_result.has_value()) {
+      return left_result;
+    }
+    auto& left_tt = left_result.value().tt_;
+    auto left_shapes = std::move(left_result.value().shapes_);
+    auto const& right_tt = right_result.value().tt_;
+    auto right_shapes = std::move(right_result.value().shapes_);
+
+    progress_tracker->status(
+        fmt::format("Merging timetables '{:d}' and '{:d}'...",
+                    l.cache_indices_[left], l.cache_indices_[right]));
+    left_tt.merge(right_tt);
+
+    /* Save snapshot */
+    progress_tracker->status(
+        fmt::format("Saving cache for '{:d}'...", l.cache_indices_[root]));
+    left_tt.write(local_cache_path / "tt.bin");
+    assert((left_shapes == nullptr) == (right_shapes == nullptr));
+    if (left_shapes == nullptr || right_shapes == nullptr) {
+      return load_result{.tt_ = left_tt, .shapes_ = nullptr};
+    }
+
+    auto shape_store = std::make_unique<shapes_storage>(
+        local_cache_path, cista::mmap::protection::WRITE);
+    shape_store->add(left_shapes.get());
+    shape_store->add(right_shapes.get());
+
+    return load_result{.tt_ = left_tt, .shapes_ = std::move(shape_store)};
+  }
+  auto const src = source_idx_t{cista::to_idx(root) - 1};
+  auto const node = source_loading_nodes[src];
+  progress_tracker->context(std::string{node.source_.tag_});
+  progress_tracker->status("Loading timetable data...");
+
+  auto result = node.load();
+  if (result.has_value()) {
+    // node.load() writes the shapes onto the filesystem
+    result.value().tt_.write(local_cache_path / "tt.bin");
+  }
+  progress_tracker->context("");
+  return result;
+}
 
 timetable load(std::vector<timetable_source> const& sources,
                finalize_options const& finalize_opt,
@@ -136,7 +281,8 @@ timetable load(std::vector<timetable_source> const& sources,
   auto const cache_metadata_path = cache_path / "meta.bin";
 
   fs::create_directories(cache_path);
-  auto chg = change_detector{};
+  auto chg = loading_tree{};
+  chg.insert_node();  // default timetable node
   for (auto const& in : sources) {
     auto const& [tag, path, local_config] = in;
 
@@ -149,47 +295,37 @@ timetable load(std::vector<timetable_source> const& sources,
     chg.source_config_hashes_.emplace_back(
         cista::hashing<loader_config>{}(local_config));
     chg.last_write_times_.emplace_back(last_write_time_t{timestamp});
+    chg.insert_node();
   }
-  auto saved_changes = change_detector{};
+  auto saved_changes = loading_tree{};
   try {
-    saved_changes = *cista::read<change_detector>(cache_metadata_path);
+    saved_changes = *cista::read<loading_tree>(cache_metadata_path);
   } catch (std::exception const& e) {
     log(log_lvl::info, "loader.load", "no cache metadata at {} found",
         cache_metadata_path);
   }
-  auto needs_recomputation = vector_map<source_idx_t, bool>{};
+  auto needs_recomputation = bitvec_map<node_idx_t>{};
+  needs_recomputation.resize(cista::to_idx(chg.n_nodes()));
   for (auto const [idx, path] : utl::enumerate(chg.source_paths_)) {
     auto const src = source_idx_t{idx};
-    needs_recomputation.emplace_back(
+    needs_recomputation.set(
+        chg.leaf_index(src),
         chg.source_paths_.size() != saved_changes.source_paths_.size() ||
-        path != saved_changes.source_paths_[src] ||
-        chg.last_write_times_[src] != saved_changes.last_write_times_[src] ||
-        chg.source_config_hashes_[src] !=
-            saved_changes.source_config_hashes_[src]);
+            path != saved_changes.source_paths_[src] ||
+            chg.last_write_times_[src] !=
+                saved_changes.last_write_times_[src] ||
+            chg.source_config_hashes_[src] !=
+                saved_changes.source_config_hashes_[src]);
   }
-  auto first_recomputed_source = std::distance(
-      needs_recomputation.begin(), utl::find(needs_recomputation, true));
 
-  auto tt = timetable{};
-  auto const progress_tracker = utl::get_active_progress_tracker();
-  for (auto i = first_recomputed_source; i >= 0; --i) {
-    if (i == 0) {
-      tt.date_range_ = date_range;
-      tt.n_sources_ = 0U;
-      register_special_stations(tt);
-      break;
-    }
-    auto const prev = i - 1;
-    auto const local_cache_path =
-        cache_path / fmt::format("tt{:d}", cista::to_idx(prev));
-    auto result = load_from_cache(local_cache_path, date_range);
-    if (!result.has_value()) {
-      continue;
-    }
-    first_recomputed_source = i;
-    tt = result.value().tt_;
-    shapes->add(result.value().shapes_.get());
-    break;
+  auto root = node_idx_t{0};
+  for (auto const [idx, in] : utl::enumerate(chg.source_paths_)) {
+    auto const src = source_idx_t{idx};
+    auto const left = root;
+    auto const right = chg.leaf_index(src);
+    root = chg.insert_node(left, right);
+    needs_recomputation.set(
+        root, needs_recomputation[left] || needs_recomputation[right]);
   }
 
   try {
@@ -199,67 +335,28 @@ timetable load(std::vector<timetable_source> const& sources,
         cache_metadata_path);
   }
 
-  auto results = vector_map<source_idx_t, load_result>{};
+  auto source_loading_nodes = vector_map<source_idx_t, loading_source_node>{};
   for (auto const [idx, in] : utl::enumerate(sources)) {
-    auto const src = source_idx_t{idx};
-    if (src < first_recomputed_source) {
-      results.emplace_back(load_result{.tt_ = timetable{}, .shapes_ = nullptr});
-      continue;
-    }
-    if (!needs_recomputation[src]) {
-      auto const local_cache_path =
-          cache_path /
-          fmt::format("tt{:d}", idx + saved_changes.source_paths_.size());
-      auto cached_timetable = timetable{};
-      auto result = load_from_cache(local_cache_path, date_range);
-      if (result.has_value()) {
-        results.emplace_back(std::move(result.value()));
-        continue;
-      }
-    }
     auto const local_cache_path =
-        cache_path / fmt::format("tt{:d}", idx + chg.source_paths_.size());
+        cache_path /
+        fmt::format("tt{:d}",
+                    chg.cache_indices_[chg.leaf_index(source_idx_t{idx})]);
     auto const& node = loading_source_node{.tmp_shapes_dir_ = local_cache_path,
                                            .source_ = in,
                                            .a_ = a,
                                            .date_range_ = date_range,
                                            .has_shapes_ = shapes != nullptr,
                                            .ignore_missing_loader_ = ignore};
-    progress_tracker->context(std::string{node.source_.tag_});
-    progress_tracker->status("Loading timetable data...");
-
-    auto result = node.load();
-    if (result.has_value()) {
-      results.emplace_back(std::move(result.value()));
-      results.back().tt_.write(local_cache_path / "tt.bin");
-    }
-    progress_tracker->context("");
+    source_loading_nodes.push_back(node);
   }
-
-  for (auto const [idx, result] : utl::enumerate(results)) {
-    auto const src = source_idx_t{idx};
-    if (src < first_recomputed_source) {
-      continue;
-    }
-    auto const local_cache_path = cache_path / fmt::format("tt{:d}", idx);
-    auto other_tt = result.tt_;
-    auto shape_store = std::move(result.shapes_);
-
-    progress_tracker->status(
-        fmt::format("Merging timetables ({:d}/{:d})...", idx, sources.size()));
-    tt.merge(other_tt);
-
-    /* Save snapshot */
-    progress_tracker->status("Saving cache...");
-    fs::create_directories(local_cache_path);
-    if (shapes != nullptr) {
-      shapes->add(shape_store.get());
-      shape_store =
-          std::make_unique<shapes_storage>(local_cache_path, shapes->mode_);
-      shape_store->add(shapes);
-    }
-    tt.write(local_cache_path / "tt.bin");
-  }
+  auto const progress_tracker = utl::get_active_progress_tracker();
+  auto const& result =
+      run_loading(chg, root, needs_recomputation, source_loading_nodes,
+                  cache_path, date_range, progress_tracker);
+  assert(result.has_value());  // Guaranteed since the default empty timetable
+                               // cannot fail to load
+  auto tt = result.value().tt_;
+  shapes->add(result.value().shapes_.get());
 
   progress_tracker->status("Finalizing").out_bounds(98.F, 100.F).in_high(1);
   finalize(tt, finalize_opt);
