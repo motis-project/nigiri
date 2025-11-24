7faba6ad86edd4babd1ba0ae0e686088bed1a612
Felix Gündling <felix@MacBookPro.fritz.box>
2025-11-24 19:27:52 +0100

    wip

--- src/loader/gtfs/stop.cc
+++ src/loader/gtfs/stop.cc
@@ -365,10 +365,12 @@ std::pair<stops_map_t, seated_transfers_map_t> read_stops(
     // Generate footpaths to connect stops in close proximity.
     hash_set<stop*> todo, done;
     for (auto const& [id, s] : stops) {
+      auto const dist_lng_degrees = geo::approx_distance_lng_degrees(s->coord_);
       for (auto const& eq : s->get_metas(stop_vec, todo, done)) {
-        auto const dist = geo::distance(s->coord_, eq->coord_);
-        auto const duration = duration_t{
-            std::max(2, static_cast<int>(std::ceil((dist / 1.4) / 60.0)))};
+        auto const dist = std::sqrt(geo::approx_squared_distance(
+            s->coord_, eq->coord_, dist_lng_degrees));
+        auto const duration = duration_t{std::max(
+            2, static_cast<int>(std::ceil((dist / kWalkSpeed) / 60.0)))};
 
         if (duration > footpath::kMaxDuration) {
           continue;
