44295d2822bac50a41d2072ab0b1a8ed0fa0b818
Felix Gündling <felix@Silvias-MacBook-Pro.local>
2025-11-30 01:01:22 +0100

    wip

--- src/loader/netex/load_timetable.cc
+++ src/loader/netex/load_timetable.cc
@@ -1,13 +1,12 @@
 #include "nigiri/loader/netex/load_timetable.h"
 
-#include <charconv>
 #include <filesystem>
-#include <numeric>
+#include <ranges>
 #include <string>
 
-#include "utl/get_or_create.h"
 #include "utl/helpers/algorithm.h"
 #include "utl/parallel_for.h"
+#include "utl/parser/arg_parser.h"
 #include "utl/pipes.h"
 #include "utl/progress_tracker.h"
 
@@ -18,16 +17,12 @@
 
 #include "pugixml.hpp"
 
-#include "nigiri/loader/get_index.h"
 #include "nigiri/loader/loader_interface.h"
-#include "nigiri/loader/register.h"
 #include "nigiri/common/sort_by.h"
 #include "nigiri/logging.h"
 #include "nigiri/timetable.h"
 
 #include "nigiri/loader/netex/intermediate.h"
-
-#include "utl/parser/arg_parser.h"
 
 namespace fs = std::filesystem;
 
@@ -42,15 +37,53 @@ struct stop {
   geo::latlng pos_;
 };
 
-struct stop_point {
-  std::string_view stop_id_;
-  bool out_allowed_;
-  bool in_allowed_;
+struct authority {
+  std::string_view name_;
+  std::string_view short_name_;
 };
 
-std::tuple<std::string_view, std::string_view, std::string_view, geo::latlng>
-format_as(stop const& x) {
-  return {x.parent_, x.id_, x.name_, x.pos_};
+struct line {
+  std::string_view name_;
+  authority const* authority_;
+};
+
+struct journey_pattern {
+  struct stop_point {
+    std::string_view id_;
+    stop const* stop_;
+    std::string_view destination_display_;
+    bool in_allowed_;
+    bool out_allowed_;
+  };
+  line const* line_;
+  direction_id_t direction_;
+  std::vector<stop_point> stop_points_;
+};
+
+template <typename T>
+concept Pointer = std::is_pointer_v<T>;
+
+template <Pointer T>
+auto format_as(T const t) {
+  return cista::to_tuple(*t);
+}
+
+template <typename T>
+concept Tuplable = cista::to_tuple_works_v<T>;
+
+template <Tuplable T>
+auto format_as(T const& t) {
+  return cista::to_tuple(t);
+}
+
+// Parses YYYY-MM-DD
+date::sys_days parse_date(utl::cstr s) {
+  utl::verify(s.len >= 10, "invalid date {}", s.view());
+  auto const date = date::year_month_day{
+      date::year{utl::parse_verify<int>(s.substr(0, utl::size(4)))},
+      date::month{utl::parse_verify<unsigned>(s.substr(5, utl::size(2)))},
+      date::day{utl::parse_verify<unsigned>(s.substr(8, utl::size(2)))}};
+  return date::sys_days{date};
 }
 
 bool is_xml_file(fs::path const& p) {
@@ -96,7 +129,7 @@ std::string_view child_attr(pugi::xml_node n,
 void load_timetable(loader_config const&,
                     source_idx_t const,
                     dir const& d,
-                    timetable&,
+                    timetable& tt,
                     hash_map<bitfield, bitfield_idx_t>& /*bitfield_indices*/,
                     assistance_times* /*assistance*/,
                     shapes_storage* /*shapes_data*/) {
@@ -124,6 +157,17 @@ void load_timetable(loader_config const&,
                             pugi::parse_default | pugi::parse_trim_pcdata);
         utl::verify(result, "Unable to parse XML buffer: {} at offset {}",
                     result.description(), result.offset);
+
+        auto authorities = hash_map<std::string_view, authority>{};
+        for (auto const s :
+             doc.select_nodes("//ResourceFrame/organisations/Authority")) {
+          auto const n = s.node();
+          authorities.emplace(
+              n.attribute("id").as_string(),
+              authority{.name_ = n.child("Name").child_value(),
+                        .short_name_ = n.child("ShortName").child_value()});
+        }
+        fmt::println("Authorities:\n\t{}", fmt::join(authorities, "\n\t"));
 
         auto stops = hash_map<std::string_view, stop>{};
         for (auto const s :
@@ -192,8 +236,152 @@ void load_timetable(loader_config const&,
         }
 
         fmt::println("Stops:\n\t{}", fmt::join(stops, "\n\t"));
-        for (auto const& [from, to] : psa) {
-          fmt::println("{} -> {}", from, format_as(*to));
+        fmt::println("PassengerStopAssignments:\n\t{}",
+                     fmt::join(psa | std::views::transform([](auto&& x) {
+                                 return std::pair{x.first, *x.second};
+                               }),
+                               "\n\t"));
+
+        auto operating_periods = hash_map<std::string_view, bitfield>{};
+        for (auto const s : doc.select_nodes(
+                 "//ServiceCalendarFrame/ServiceCalendar/operatingPeriods/"
+                 "UicOperatingPeriod")) {
+          auto const sn = s.node();
+          auto const from = parse_date(sn.child("FromDate").child_value());
+          auto const to = parse_date(sn.child("ToDate").child_value());
+          auto const bits =
+              std::string_view{sn.child("ValidDayBits").child_value()};
+
+          utl::verify((to - from).count() + 1U == bits.size(),
+                      "from={}, to={} != n_bits={}", from, to, bits.size());
+
+          auto bf = bitfield{};
+          auto day = std::max(from, tt.internal_interval_days().from_);
+          for (; day <= to && day < tt.internal_interval_days().to_;
+               day += std::chrono::days{1}) {
+            auto const tt_day_idx =
+                (day - tt.internal_interval_days().from_).count();
+            if (tt_day_idx >= 0 && tt_day_idx < static_cast<int>(bf.size())) {
+              bf.set(tt_day_idx, bits.at((day - from).count()));
+            }
+          }
+
+          operating_periods.emplace(sn.attribute("id").as_string(), bf);
+        }
+        fmt::println("UicOperatingPeriods:\n\t{}",
+                     fmt::join(operating_periods, "\n\t"));
+
+        auto days = hash_map<std::string_view, bitfield const*>{};
+        for (auto const x : doc.select_nodes(
+                 "//ServiceCalendarFrame/ServiceCalendar/dayTypeAssignments/"
+                 "DayTypeAssignment")) {
+          auto const n = x.node();
+          auto const op_period = child_attr(n, "OperatingPeriodRef", "ref");
+          auto const day_type = child_attr(n, "DayTypeRef", "ref");
+
+          auto const it = operating_periods.find(op_period);
+          if (it == end(operating_periods)) {
+            log(log_lvl::error, "netex.DayTypeAssignment",
+                "OperatingPeriodRef=\"{}\" not found", op_period);
+            continue;
+          }
+
+          days.emplace(day_type, &it->second);
+        }
+        fmt::println("DayTypes:\n\t{}",
+                     fmt::join(days | std::views::transform([](auto&& x) {
+                                 return std::pair{x.first, *x.second};
+                               }),
+                               "\n\t"));
+
+        auto lines = hash_map<std::string_view, line>{};
+        for (auto const l : doc.select_nodes("//ServiceFrame/lines/Line")) {
+          auto const n = l.node();
+          auto const it =
+              authorities.find(child_attr(n, "AuthorityRef", "ref"));
+          lines.emplace(
+              n.attribute("id").as_string(),
+              line{.name_ = n.child("Name").child_value(),
+                   .authority_ =
+                       it == end(authorities) ? nullptr : &it->second});
+        }
+
+        auto destination_displays =
+            hash_map<std::string_view, std::string_view>{};
+        for (auto const display : doc.select_nodes(
+                 "//ServiceFrame/destinationDisplays/DestinationDisplay")) {
+          auto const n = display.node();
+          destination_displays.emplace(n.attribute("id").as_string(),
+                                       n.child("FrontText").child_value());
+        }
+
+        auto journey_patterns = hash_map<std::string_view, journey_pattern>{};
+        for (auto const j : doc.select_nodes(
+                 "//ServiceFrame/journeyPatterns/ServiceJourneyPattern")) {
+          auto const n = j.node();
+
+          auto stop_points = std::vector<journey_pattern::stop_point>{};
+          for (auto const sp : n.child("pointsInSequence").children()) {
+            stop_points.push_back(journey_pattern::stop_point{
+                .id_ = sp.attribute("id").as_string(),
+                .stop_ = psa.at(child_attr(sp, "ScheduledStopPointRef", "ref")),
+                .destination_display_ = destination_displays.at(
+                    child_attr(sp, "DestinationDisplayRef", "ref")),
+                .in_allowed_ =
+                    std::string_view{sp.child("ForBoarding").child_value()} ==
+                    "true",
+                .out_allowed_ =
+                    std::string_view{sp.child("ForAlighting").child_value()} ==
+                    "true"});
+          }
+
+          journey_patterns.emplace(
+              n.attribute("id").as_string(),
+              journey_pattern{
+                  .line_ = &lines.at(
+                      child_attr(n.child("RouteView"), "LineRef", "ref")),
+                  .direction_ =
+                      direction_id_t{
+                          child_attr(n, "DirectionRef", "ref").ends_with("1::")
+                              ? 0
+                              : 1},
+                  .stop_points_ = stop_points});
+        }
+        fmt::println(
+            "JourneyPatterns:\n\t{}",
+            fmt::join(
+                journey_patterns |
+                    std::views::transform(
+                        [](std::pair<std::string_view, journey_pattern> const&
+                               x) {
+                          return std::tuple{
+                              x.first, x.second.line_->name_,
+                              x.second.line_->authority_->name_,
+                              x.second.stop_points_ |
+                                  std::views::transform(
+                                      [](journey_pattern::stop_point const s) {
+                                        return std::tuple{s.stop_->name_,
+                                                          s.stop_->id_};
+                                      })};
+                        }),
+                "\n\t"));
+
+        for (auto const s : doc.select_nodes(
+                 "//TimetableFrame/vehicleJourneys/ServiceJourney")) {
+          auto const n = s.node();
+          auto const trip_nr =
+              n.select_node("keyList/KeyValue/Key[text() = 'TripNr']")
+                  .parent()
+                  .child("Value")
+                  .child_value();
+          auto const day_type =
+              n.select_node("dayTypes/DayTypeRef/@ref").attribute().as_string();
+          std::cout << "trip_nr=" << trip_nr
+                    << ", day_type=" << *days.at(day_type) << "\n";
+
+          // auto const transport_mode = n.child("TransportMode").child_value();
+          // auto const transport_sub_mode =
+          //   n.child("TransportSubmode").child("RailSubmodule").child_value();
         }
 
         auto im = intermediate{};
--- test/loader/netex/psa_test.cc
+++ test/loader/netex/psa_test.cc
@@ -1,10 +1,13 @@
 #include "nigiri/loader/netex/load_timetable.h"
 
+#include "utl/parser/cstr.h"
+
 #include "gtest/gtest.h"
 
 using namespace nigiri;
 using namespace nigiri::loader;
 using namespace nigiri::loader::netex;
+using namespace date;
 
 constexpr auto kXml = R"(
 # netex.xml
@@ -13,6 +16,51 @@ constexpr auto kXml = R"(
   <dataObjects>
     <CompositeFrame id="DE::CompositeFrame_EU_PI_LINE_OFFER:123-FBBUS-FB-1" version="1763819179">
       <frames>
+        <ResourceFrame id="DE::ResourceFrame_EU_PI_COMMON:DBDB-800486-S4" version="1763819179">
+          <organisations>
+            <Authority id="DE::Authority:151::" version="1763819179">
+              <PublicCode>DBDB</PublicCode>
+              <Name>DB_Gesamtnetz der deutschen Bahn</Name>
+              <ShortName>DBDB</ShortName>
+              <LegalName>DB_Gesamtnetz der deutschen Bahn</LegalName>
+              <ContactDetails/>
+              <OrganisationType>authority</OrganisationType>
+            </Authority>
+          </organisations>
+        </ResourceFrame>
+
+        <ServiceCalendarFrame id="DE::ServiceCalendarFrame_EU_PI_CALENDAR:DBDB-800486-S4" version="1763819179">
+          <ServiceCalendar id="DE::ServiceCalendar:27522::" version="1763819179">
+
+            <operatingPeriods>
+              <UicOperatingPeriod id="DE::UicOperatingPeriod:249613::" version="1763819179">
+                <FromDate>2025-12-14T00:00:00</FromDate>
+                <ToDate>2026-06-13T00:00:00</ToDate>
+                <ValidDayBits>11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111</ValidDayBits>
+              </UicOperatingPeriod>
+              <UicOperatingPeriod id="DE::UicOperatingPeriod:249614::" version="1763819179">
+                <FromDate>2025-12-14T00:00:00</FromDate>
+                <ToDate>2025-12-14T00:00:00</ToDate>
+                <ValidDayBits>1</ValidDayBits>
+              </UicOperatingPeriod>
+            </operatingPeriods>
+
+            <dayTypeAssignments>
+              <DayTypeAssignment id="DE::DayTypeAssignment:249613::" order="1" version="1763819179">
+                <OperatingPeriodRef ref="DE::UicOperatingPeriod:249613::" version="1763819179"/>
+                <DayTypeRef ref="DE::DayType:249613::" version="1763819179"/>
+                <isAvailable>true</isAvailable>
+              </DayTypeAssignment>
+              <DayTypeAssignment id="DE::DayTypeAssignment:249614::" order="2" version="1763819179">
+                <OperatingPeriodRef ref="DE::UicOperatingPeriod:249614::" version="1763819179"/>
+                <DayTypeRef ref="DE::DayType:249614::" version="1763819179"/>
+                <isAvailable>true</isAvailable>
+              </DayTypeAssignment>
+            </dayTypeAssignments>
+
+          </ServiceCalendar>
+        </ServiceCalendarFrame>
+
         <SiteFrame id="DE::SiteFrame_EU_PI_STOP:123-FBBUS-FB-1" version="1763819179">
           <stopPlaces>
             <StopPlace id="DE::StopPlace:380019319_1000::" version="1763819179">
@@ -122,7 +170,42 @@ constexpr auto kXml = R"(
             </Quay>
           </stopPlaces>
         </SiteFrame>
+
         <ServiceFrame id="DE::ServiceFrame_EU_PI_NETWORK:123-FBBUS-FB-1" version="1763819179">
+          <directions>
+            <Direction id="DE::Direction:1::" version="1763819179"/>
+            <Direction id="DE::Direction:2::" version="1763819179"/>
+          </directions>
+
+          <lines>
+            <Line id="DE::Line:162620::" version="1763819179">
+              <Name>S4</Name>
+              <ShortName>S4</ShortName>
+              <TransportMode>rail</TransportMode>
+              <PublicCode>S4</PublicCode>
+              <PrivateCode>S4</PrivateCode>
+              <AuthorityRef ref="DE::Authority:151::" version="1763819179"/>
+              <additionalOperators>
+                <OperatorRef ref="DE::Operator:11662::" version="1763819179"/>
+              </additionalOperators>
+              <allowedDirections>
+                <AllowedLineDirection id="DE::AllowedLineDirection:46777::" version="1763819179">
+                  <DirectionRef ref="DE::Direction:1::" version="1763819179"/>
+                </AllowedLineDirection>
+                <AllowedLineDirection id="DE::AllowedLineDirection:46778::" version="1763819179">
+                  <DirectionRef ref="DE::Direction:2::" version="1763819179"/>
+                </AllowedLineDirection>
+              </allowedDirections>
+            </Line>
+          </lines>
+
+          <destinationDisplays>
+            <DestinationDisplay id="DE::DestinationDisplay:35217::" version="1763819179">
+              <SideText>Oschatz</SideText>
+              <FrontText>Oschatz</FrontText>
+            </DestinationDisplay>
+          </destinationDisplays>
+
           <stopAssignments>
             <PassengerStopAssignment id="DE::PassengerStopAssignment:150509::" order="30" version="1763819179">
               <ScheduledStopPointRef ref="DE::ScheduledStopPoint:101376501_123_::" version="1763819179"/>
@@ -134,15 +217,92 @@ constexpr auto kXml = R"(
               <QuayRef ref="DE::Quay:12297401_1000::" version="1763819179"/>
             </PassengerStopAssignment>
           </stopAssignments>
+
+          <journeyPatterns>
+            <ServiceJourneyPattern id="DE::ServiceJourneyPattern:296311804_0::" version="1763819179">
+              <RouteView id="DE::RouteView:296311804_1::">
+                <LineRef ref="DE::Line:162620::" version="1763819179"/>
+              </RouteView>
+              <DirectionRef ref="DE::Direction:1::" version="1763819179"/>
+              <pointsInSequence>
+                <StopPointInJourneyPattern id="DE::StopPointInJourneyPattern:296311804_1_0::" order="1" version="1763819179">
+                  <ScheduledStopPointRef ref="DE::ScheduledStopPoint:101376501_123_::" version="1763819179"/>
+                  <ForAlighting>true</ForAlighting>
+                  <ForBoarding>true</ForBoarding>
+                  <DestinationDisplayRef ref="DE::DestinationDisplay:35217::" version="1763819179"/>
+                  <ChangeOfDestinationDisplay>false</ChangeOfDestinationDisplay>
+                  <noticeAssignments>
+                    <NoticeAssignment id="DE::NoticeAssignment:87899403::" order="1" version="1763819179">
+                      <Notice id="DE::Notice:478_227594::" version="1763819179">
+                        <Text lang="de-DE">Fahrradmitnahme begrenzt möglich</Text>
+                        <PublicCode>F2G</PublicCode>
+                      </Notice>
+                    </NoticeAssignment>
+                  </noticeAssignments>
+                </StopPointInJourneyPattern>
+              </pointsInSequence>
+            </ServiceJourneyPattern>
+          </journeyPatterns>
+
         </ServiceFrame>
+
+        <TimetableFrame id="DE::TimetableFrame_EU_PI_TIMETABLE:DBDB-800486-S4" version="1763819179">
+          <vehicleJourneys>
+            <ServiceJourney id="DE::ServiceJourney:3024509099_0::" version="1763819179">
+              <keyList>
+                <KeyValue>
+                  <Key>TripNr</Key>
+                  <Value>037400</Value>
+                </KeyValue>
+              </keyList>
+              <TransportMode>rail</TransportMode>
+              <TransportSubmode>
+                <RailSubmode>suburbanRailway</RailSubmode>
+              </TransportSubmode>
+              <DepartureTime>04:24:00</DepartureTime>
+              <JourneyDuration>PT1H46M</JourneyDuration>
+              <dayTypes>
+                <DayTypeRef ref="DE::DayType:249613::" version="1763819179"/>
+              </dayTypes>
+              <ServiceJourneyPatternRef ref="DE::ServiceJourneyPattern:296311804_0::" version="1763819179"/>
+              <VehicleTypeRef ref="DE::VehicleType:3834::" version="1763819179"/>
+              <passingTimes>
+                <TimetabledPassingTime id="DE::TimetabledPassingTime:81739562::" version="1763819179">
+                  <StopPointInJourneyPatternRef ref="DE::StopPointInJourneyPattern:296311804_1_0::" version="1763819179"/>
+                  <DepartureTime>04:24:00</DepartureTime>
+                </TimetabledPassingTime>
+                <TimetabledPassingTime id="DE::TimetabledPassingTime:81739563::" version="1763819179">
+                  <StopPointInJourneyPatternRef ref="DE::StopPointInJourneyPattern:296311804_1_1::" version="1763819179"/>
+                  <ArrivalTime>04:33:00</ArrivalTime>
+                  <DepartureTime>04:33:00</DepartureTime>
+                </TimetabledPassingTime>
+                <TimetabledPassingTime id="DE::TimetabledPassingTime:81739591::" version="1763819179">
+                  <StopPointInJourneyPatternRef ref="DE::StopPointInJourneyPattern:296311804_1_29::" version="1763819179"/>
+                  <ArrivalTime>06:10:00</ArrivalTime>
+                </TimetabledPassingTime>
+              </passingTimes>
+            </ServiceJourney>
+          </vehicleJourneys>
+        </TimetableFrame>
       </frames>
     </CompositeFrame>
   </dataObjects>
 </PublicationDelivery>
 )";
 
+namespace nigiri::loader::netex {
+sys_days parse_date(utl::cstr);
+}
+
+TEST(netex, parse_date) {
+  EXPECT_EQ(sys_days{2025_y / December / 14}, parse_date("2025-12-14T00:00"));
+  EXPECT_EQ(sys_days{2025_y / December / 14}, parse_date("2025-12-14"));
+}
+
 TEST(netex, psa) {
   auto global_bitfields = hash_map<bitfield, bitfield_idx_t>{};
   auto tt = timetable{};
+  tt.date_range_ = {sys_days{2025_y / December / 14},
+                    sys_days{2026_y / December / 14}};
   load_timetable({}, {}, mem_dir::read(kXml), tt, global_bitfields);
 }
