e2fd8bcebb53537dba864c0414be3c5a2841115f
Jonah Brüchert <jbb@kaidan.im>
2026-05-07 16:57:48 +0200

    Implement run_stop::get_route_id_idx

--- include/nigiri/timetable.h
+++ include/nigiri/timetable.h
@@ -500,7 +500,7 @@ struct timetable {
   };
 
   vecvec<ticketing_link_idx_t, ticketing_link> ticketing_links_;
-  hash_map<provider_idx_t, ticketing_link_idx_t> ticketing_agencies;
+  hash_map<provider_idx_t, ticketing_link_idx_t> ticketing_agencies_;
   hash_map<route_id_idx_t, ticketing_link_idx_t> ticketing_routes_;
 };
 
