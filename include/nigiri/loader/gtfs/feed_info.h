86b66129fa8dda4b7893f3e6a9f3ed5573e0b204
Felix Gündling <felixguendling@gmail.com>
2026-07-17 21:11:12 +0200

    use default_lang

--- include/nigiri/loader/gtfs/feed_info.h
+++ include/nigiri/loader/gtfs/feed_info.h
@@ -4,11 +4,11 @@
 
 namespace nigiri::loader::gtfs {
 
-struct feed_info_test {
+struct feed_info {
   std::optional<date::sys_days> feed_end_date_;
-  std::string default_lang_;
+  std::string feed_lang_;
 };
 
-feed_info_test read_feed_info(std::string_view);
+feed_info read_feed_info(std::string_view);
 
 }  // namespace nigiri::loader::gtfs
